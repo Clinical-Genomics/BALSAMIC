991391829db010cd800bab300fa9068489ab2dab
hassanfa <hassan.foroughi@gmail.com>
2018-04-26 15:14:53 +0200

    Updated fastq path in rules and analysis wrapper

--- BALSAMIC/workflows/run_analysis.py
+++ BALSAMIC/workflows/run_analysis.py
@@ -8,10 +8,18 @@ import json
 
 
 def get_sample_name(json_in):
+    """
+    Get sample name from input json file
+    """
 
-    json_in = json.load(open(json_in))
+    try:
+      with open(os.path.abspath(json_in), "r") as fn:
+        sample_json = json.load(fn)
+        sample_name = sample_json["analysis"]["sample_id"]
+    except OSError:
+      print("Couldn't load json file or file path is not absolute")
 
-    return json_in["analysis"]["sample_id"]
+    return sample_name 
 
 
 @click.command(
@@ -23,17 +31,17 @@ def get_sample_name(json_in):
     type=click.Path(),
     help='Snakefile required for snakemake to function.')
 @click.option(
-    '-c',
-    '--config-sample',
+    '-s',
+    '--sample-config',
     required=True,
     type=click.Path(),
     help='Sample json config file.')
 @click.option(
-    '-s',
-    '--config-slurm',
+    '-c',
+    '--cluster-config',
     required=True,
     type=click.Path(),
-    help='Slurm config json file.')
+    help='SLURM config json file.')
 @click.option(
     '-l',
     '--log-file',
@@ -62,8 +70,8 @@ def get_sample_name(json_in):
 def run_analysis(
         context,
         snake_file,
-        config_sample,
-        config_slurm,
+        sample_config,
+        cluster_config,
         run_analysis,
         log_file,
         force_all,
@@ -77,14 +85,14 @@ def run_analysis(
     shellcmd = ["snakemake --immediate-submit -j 99 --notemp -p"]
     shellcmd.append(
         "--jobname " +
-        get_sample_name(config_sample) +
+        get_sample_name(sample_config) +
         ".{rulename}.{jobid}.sh")
     shellcmd.append("--snakefile " + snake_file)
-    shellcmd.append("--configfile " + config_sample)
-    shellcmd.append("--cluster-config " + config_slurm)
+    shellcmd.append("--configfile " + sample_config)
+    shellcmd.append("--cluster-config " + cluster_config)
     shellcmd.append(
         "--cluster 'python3 runSbatch.py --sample-config " +
-        config_sample +
+        sample_config +
         " {dependencies} '")
 
     if not run_analysis:
