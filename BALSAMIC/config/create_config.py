7f2d2839353b3ea062004f7893563af2ce7e46b8
hassanfa <hassan.foroughi@gmail.com>
2018-05-13 16:13:03 +0200

    bed file input is now a required optoion

--- BALSAMIC/config/create_config.py
+++ BALSAMIC/config/create_config.py
@@ -44,12 +44,23 @@ def get_config(config_name):
 
     try:
         config_file = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), config_name, '.json')
+            os.path.dirname(os.path.abspath(__file__)), config_name + '.json')
     except OSError:
         print("Couldn't locate config file" + config_name + ".json")
 
     return config_file
 
+def set_panel_bed(json_out, panel_bed):
+    """
+    Set panel path in config file
+    """
+    try:
+        json_out['path']['panel'] = os.path.split(os.path.abspath(panel_bed))[0] + "/"
+        json_out['bed']['variant_panel'] = os.path.split(os.path.abspath(panel_bed))[1]
+    except OSError:
+        print("Couldn't locate bed file" + panel_bed)
+
+    return json_out
 
 @click.command(
     "create_config",
@@ -79,6 +90,12 @@ def get_config(config_name):
     type=click.Path(),
     help='Reference config file.')
 @click.option(
+    '-p',
+    '--panel-bed',
+    required=True,
+    type=click.Path(),
+    help='Panel bed file for variant calling.')
+@click.option(
     '-s',
     '--sample-config',
     required=True,
@@ -92,7 +109,7 @@ def get_config(config_name):
     help='Output a json config file ready to be imported for run-analysis')
 @click.pass_context
 def create_config(context, analysis_config, install_config, sample_config,
-                  reference_config, output_config):
+                  reference_config, panel_bed, output_config):
     """
     Prepares a config file for balsamic run_analysis. For now it is just treating json as dictionary and merging them as
 it is. So this is just a placeholder for future.
@@ -106,4 +123,8 @@ it is. So this is just a placeholder for future.
 
     json_out = merge_json(analysis_config, sample_config, reference_config,
                           install_config)
+    
+    if panel_bed:
+        json_out = set_panel_bed(json_out, panel_bed)
+
     write_json(json_out, output_config)
