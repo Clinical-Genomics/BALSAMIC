a32abf33ac66258850090da2418326e8aea530ff
hassanfa <hassan.foroughi@gmail.com>
2018-05-28 18:11:24 +0200

    three mandatory values added

--- BALSAMIC/workflows/runSbatch.py
+++ BALSAMIC/workflows/runSbatch.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
-"""
-Submit this clustering script for sbatch to snakemake with:
-    snakemake --forceall --snakefile Snakefile \
-      --cluster-config config_cluster.json \
-      -j 9999 \
-      --cluster 'python3 runSbatch.py --config config_cluster.json {dependencies}' \
-      --immediate-submit --notemp
-"""
 import sys
 import re
 import os
@@ -14,28 +6,32 @@ import json
 import argparse
 from snakemake.utils import read_job_properties
 
-# CLI commands and decorators
-from BALSAMIC.tools.cli_utils import createDir
-
-parser = argparse.ArgumentParser(description='Process script and dependecies.')
+parser = argparse.ArgumentParser(
+    description=
+    'This is an internal script and should be invoked independently. This script gets a list of arugments (see --help) and submits a job to slurm as afterok dependency. All inputs are mandatory!'
+)
 parser.add_argument(
     "dependencies",
     nargs="*",
     help="{{dependencies}} string given by snakemake\n")
 parser.add_argument(
     "snakescript",
-    help="Snakemake generated shell script with commands to execute snakemake rule\n")
-# parser.add_argument(
-#    "--cluster-config",
-#    help="Config file to read sbatch settings from.")
+    help=
+    "Snakemake generated shell script with commands to execute snakemake rule\n"
+)
 parser.add_argument(
-    "--sample-config",
-    help="Config file to read sbatch settings from.")
+    "--sample-config", help="Config file to read sbatch settings from.")
+parser.add_argument("--dir-log", help="Log directory")
+parser.add_argument("--dir-result", help="Result directory")
+parser.add_argument("--dir-script", help="Script directory")
 args = parser.parse_args()
 
 sample_config = json.load(open(args.sample_config))
 jobscript = args.snakescript
 job_properties = read_job_properties(jobscript)
+logpath = args.dir_log
+scriptpath = args.dir_script
+resultpath = args.dir_result
 
 time = job_properties["cluster"]["time"]
 cpu = job_properties["cluster"]["n"]
@@ -43,24 +39,8 @@ account_slurm = job_properties["cluster"]["account"]
 mail_type = job_properties["cluster"]["mail_type"]
 mail_user = job_properties["cluster"]["mail_user"]
 
-logpath = os.path.join(
-    sample_config["analysis"]["analysis_dir"],
-    sample_config["analysis"]["sample_id"],
-    sample_config["analysis"]["log"])
-scriptpath = os.path.join(
-    sample_config["analysis"]["analysis_dir"],
-    sample_config["analysis"]["sample_id"],
-    sample_config["analysis"]["script"])
-resultpath = os.path.join(
-    sample_config["analysis"]["analysis_dir"],
-    sample_config["analysis"]["sample_id"],
-    sample_config["analysis"]["result"])
+os.system('cp ' + jobscript + ' ' + scriptpath + '/')
 
-createDir(logpath)
-createDir(scriptpath)
-os.path.makedirs(resultpath, exist_ok=TRUE)
-
-os.system('cp ' + jobscript + ' ' + scriptpath)
 scriptname = jobscript.split("/")
 scriptname = scriptname[-1]
 jobscript = os.path.join(scriptpath, scriptname)
@@ -68,7 +48,13 @@ jobscript = os.path.join(scriptpath, scriptname)
 output_log = logpath + scriptname + ".out"
 error_log = logpath + scriptname + ".err"
 cmdline = 'sbatch -A {account} -n {n} -t {time} --qos=low -o {output_log} -e {error_log} --mail-type {mail_type} --mail-user {mail_user}'.format(
-    n=cpu, time=time, output_log=output_log, error_log=error_log, account=account_slurm, mail_type=mail_type, mail_user=mail_user)
+    n=cpu,
+    time=time,
+    output_log=output_log,
+    error_log=error_log,
+    account=account_slurm,
+    mail_type=mail_type,
+    mail_user=mail_user)
 
 cmdline += " "
 
