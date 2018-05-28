f8a338ec92b12a8b1ef7520584b572c8d56ba017
hassanfa <hassan.foroughi@gmail.com>
2018-05-28 15:40:40 +0200

    recursively create a directory by incrementing a digit at the end

--- BALSAMIC/tools/cli_utils.py
+++ BALSAMIC/tools/cli_utils.py
@@ -1,3 +1,5 @@
+import os
+
 def add_doc(docstring):
     """
     A decorator for adding docstring. Taken shamelessly from stackexchange. 
@@ -7,3 +9,17 @@ def add_doc(docstring):
         return func
 
     return document
+
+def createDir(path):
+    if os.path.isdir(path):
+        basepath = os.path.basename(path)
+        basepath_number = 0
+        if "." in basepath:
+            basepath_number = int(basepath.split(".")[1])
+        basepath_string = basepath.split(".")[0]
+        basepath_number += 1
+        path = os.path.join(os.path.dirname(path), ".".join([basepath_string,str(basepath_number)]))
+        createDir(path)
+    else:
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        return True
