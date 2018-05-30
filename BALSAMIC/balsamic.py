ad218df2cb005382eb5aaa94520e1a427ff7c02f
hassanfa <hassan.foroughi@gmail.com>
2018-05-30 11:13:03 +0200

    Added dev_report subcommand to balsamic main cli

--- BALSAMIC/balsamic.py
+++ BALSAMIC/balsamic.py
@@ -12,6 +12,7 @@ import json
 from BALSAMIC.install import install_env as install_command
 from BALSAMIC.workflows import run_analysis as analysis_command
 from BALSAMIC.config import create_config as config_command
+from BALSAMIC.dev_report import report as report_command
 
 # CLI commands and decorators
 from BALSAMIC.tools.cli_utils import add_doc as doc
@@ -27,7 +28,7 @@ from BALSAMIC import __version__
 def cli(context):
     pass
 
-
 cli.add_command(install_command)
 cli.add_command(analysis_command)
 cli.add_command(config_command)
+cli.add_command(report_command)
