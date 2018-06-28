0ead651eba489f931d7715bc2a577d8eb92f1fee
hassanfa <hassan.foroughi@gmail.com>
2018-06-28 09:02:52 +0200

    picard source jar was not linked properly

--- install.sh
+++ install.sh
@@ -77,6 +77,7 @@ picard_PATH=BALSAMIC/assests/picard-2.18.5-4-g63d860e-SNAPSHOT-all.jar
 picard_conda_env=`python -c 'from BALSAMIC.tools import get_conda_env; print(get_conda_env("BALSAMIC_env.yaml", "picard"))'`
 picard_destination=`conda env export -n ${picard_conda_env} | grep prefix | cut -d" " -f 2`
 cp $picard_PATH ${picard_destination}/share/
+ln -s ${picard_destination}/share/picard-2.18.5-4-g63d860e-SNAPSHOT-all.jar  ${picard_destination}/share/picard-2.18.5.jar
 
 unset B_RED
 unset B_GRN
