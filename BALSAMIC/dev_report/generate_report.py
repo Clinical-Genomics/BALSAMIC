3a0a2803f4f756e331f4a1826de756471baf3fa5
hassanfa <hassan.foroughi@gmail.com>
2018-09-04 20:53:24 +0200

    clutter cleaned up, first draft nomination

--- BALSAMIC/dev_report/generate_report.py
+++ BALSAMIC/dev_report/generate_report.py
@@ -16,63 +16,6 @@ def extractSampleName(config):
     return sample_name
 
 
-def extractVCFstat():
-
-    variant_stat = ["21", "15479624", "G", "T", "SNV", "300/0.01"]
-
-    return variant_stat
-
-
-def countReads(config, flag):
-
-    if flag == 'fastq':
-        sample_read = config["Metrics"]["fastq"]
-    elif flag == 'bam':
-        sample_read = config["Metrics"]["BAM"]
-    elif flag == 'bam_rmdup':
-        sample_read = config["Metrics"]["BAM_rmdup"]
-    else:
-        sample_read = "NA"
-
-    return sample_read
-
-
-def countVCF(config, var_caller, flag):
-
-    if flag == 'all':
-        vcf_count = config["vcf"][var_caller]["All"]
-    elif flag == 'pass':
-        vcf_count = config["vcf"][var_caller]["Passed"]
-    elif flag == 'annot':
-        vcf_count = config["vcf"][var_caller]["Annot"]["Total"]
-    elif flag == 'annot_type':
-        vcf_count = config["vcf"][var_caller]["Annot"]["misense"] + \
-                    "missense and " + \
-                    config["vcf"][var_caller]["Annot"]["sense"] + \
-                    "sense"
-    elif flag == 'pathological':
-        vcf_count = config["vcf"][var_caller]["Annot"]["pathological"]
-    else:
-        vcf_count = str("NA")
-
-    return vcf_count
-
-
-def varCallerSummary(config, var_caller):
-
-    row = [var_caller]
-
-    row.append(config["vcf"][var_caller]["All"])
-
-    row.append(config["vcf"][var_caller]["Passed"])
-
-    row.append(config["vcf"][var_caller]["Annot"]["misense"])
-    row.append(config["vcf"][var_caller]["Annot"]["sense"])
-    row.append(config["vcf"][var_caller]["Annot"]["pathological"])
-
-    return row
-
-
 @click.command("report", short_help="Report generator for workflow results")
 @click.option(
     '-j',
@@ -125,32 +68,35 @@ def report(context, json_report, json_varreport, rulegraph_img):
 
     with doc.create(Section(title='Analysis report', numbering=True)):
         with doc.create(
-                Subsection('Summary of alignment report', numbering=True)):
+                Subsection(
+                    'Summary of variants and variant callers',
+                    numbering=True)):
             doc.append(
-                "Placeholder for text about BAM alignment. Here comes the info on reads, "
+                "Placeholder for text about BAM alignment metrics and variant callers. Here comes the info on reads, "
                 +
                 "QC metrics, align metrics, and general sample information. preferabily in table format."
             )
             doc.append("\n")
-            fmt = "p{2cm}p{3cm}p{3cm}p{3cm}p{3cm}p{3cm}"
-            with doc.create(Tabular(fmt, pos="H")) as data_table:
-                header_row1 = [""]
-                for i in var_config["filters"]:
-                    header_row1.append(var_config["filters"][i]["name"])
-                print(header_row1)
-                data_table.add_hline()
-                data_table.add_row(header_row1, mapper=[bold])
-                data_table.add_hline()
-                data_table.add_empty_row()
-                column = list(var_config["filters"][next(
-                    iter(var_config["filters"]))]["TUMOR"].keys())
-                for i in column:
-                    row = [i]
-                    for j in var_config["filters"]:
-                        row.append(var_config["filters"][j]["TUMOR"][i])
-                    print(row)
-                    data_table.add_row(row)
-            data_table.add_hline()
+
+            summary_tables = ["TMB", "VarClass", "VarCaller", "VarCallerClass"]
+            for i in summary_tables:
+
+                shellcmd = [
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), "..",
+                        "R_scripts/VariantReport.R")
+                ]
+                shellcmd.extend([
+                    "--infile", sample_config["vcf"]["merged"]["SNV"],
+                    "--genomeSize", sample_config["bed"]["genome_size"],
+                    "--type", "latex", "--mode", i
+                ])
+
+                outTab = subprocess.check_output(shellcmd)
+                doc.append(
+                    NoEscape(outTab.decode('utf-8').replace(
+                        "\\centering", "\\small")))
+            doc.append(NoEscape(r'\normalsize'))
 
         with doc.create(Subsection("Summary of MVL report", numbering=True)):
             doc.append(
@@ -160,7 +106,7 @@ def report(context, json_report, json_varreport, rulegraph_img):
             )
             outVar = dict()
             outCov = dict()
-            for i in ["set_2"]:#var_config["filters"]:
+            for i in var_config["filters"]:
                 shellcmd = [
                     os.path.join(
                         os.path.dirname(os.path.abspath(__file__)), "..",
@@ -185,18 +131,20 @@ def report(context, json_report, json_varreport, rulegraph_img):
                     if outVar[i] != b"FALSE\n":
                         shellcmd.extend(["--exportGene", "T"])
                         print(" ".join(shellcmd))
-                        genes = subprocess.check_output(shellcmd).decode('utf-8')
+                        genes = subprocess.check_output(shellcmd).decode(
+                            'utf-8')
+                        genes = genes.rstrip("\n\r")
                         shellcmd = [
                             os.path.join(
-                                os.path.dirname(os.path.abspath(__file__)), "..",
-                                "R_scripts/CoverageRep.R")
+                                os.path.dirname(os.path.abspath(__file__)),
+                                "..", "R_scripts/CoverageRep.R")
                         ]
                         shellcmd.extend([
                             "--infile", sample_config["bed"]["exon_cov"],
-                            "--genename", genes, "--type", "latex"])
-                        print(genes)
+                            "--genename", genes, "--type", "latex"
+                        ])
                         print(" ".join(shellcmd))
-                        outCov[i] = subprocess.check_output(shellcmd) 
+                        outCov[i] = subprocess.check_output(shellcmd)
                         #doc.append(NoEscape(r'\begin{landscape}'))
                         #longtable instead of tabular makes the table span multiple pages, but the header doesn't span. Occasionally
                         #the alignment also is messed up. There must be a hidden package conflict OR general alignment issues.
@@ -210,14 +158,8 @@ def report(context, json_report, json_varreport, rulegraph_img):
                         #doc.append(NoEscape(r'\end{landscape}'))
                     else:
                         doc.append("No variants were found for this filter")
-                break
 
                 doc.append(NoEscape(r'\normalsize'))
-
-        with doc.create(
-                Subsection(
-                    'Summary of variant calling report', numbering=True)):
-            doc.append("Variant calling reports and summaries go here")
 
         with doc.create(Subsection('Analysis pipeline')):
             with doc.create(Figure(position='h!')) as pipeline_img:
@@ -231,7 +173,6 @@ def report(context, json_report, json_varreport, rulegraph_img):
                 header_row1 = [""]
                 for i in var_config["filters"]:
                     header_row1.append(var_config["filters"][i]["name"])
-                print(header_row1)
                 data_table.add_hline()
                 data_table.add_row(header_row1, mapper=[bold])
                 data_table.add_hline()
@@ -242,70 +183,17 @@ def report(context, json_report, json_varreport, rulegraph_img):
                     row = [i]
                     for j in var_config["filters"]:
                         row.append(var_config["filters"][j]["TUMOR"][i])
-                    print(row)
                     data_table.add_row(row)
-                
+
                 row = ["MVL"]
                 for i in var_config["filters"]:
                     row.append(var_config["filters"][i]["in_mvl"])
-                
+
                 row = ["Variantcallers"]
                 for i in var_config["filters"]:
-                    row.append("\n".join(var_config["filters"][i]["variantcaller"]))
+                    row.append("\n".join(
+                        var_config["filters"][i]["variantcaller"]))
                 data_table.add_row(row)
                 data_table.add_hline()
 
     doc.generate_pdf('full', clean_tex=False)
-
-
-#            for var_caller in sample_config["vcf"]:
-#                with doc.create(Subsubsection(var_caller, numbering=False)):
-#                    doc.append("Variant calling resulted in " + countVCF(
-#                        config=sample_config, var_caller=var_caller, flag="all") +
-#                               " variants, of which ")
-#                    doc.append(
-#                        countVCF(
-#                            config=sample_config, var_caller=var_caller, flag="pass")
-#                        + " passed our filters (FILTER=PASS). ")
-#                    doc.append("From this list " + countVCF(
-#                        config=sample_config, var_caller=var_caller, flag="annot") +
-#                               " were annotated using VEP and SNPeff. ")
-#                    doc.append("In summary, there were " + countVCF(
-#                        config=sample_config,
-#                        var_caller=var_caller,
-#                        flag="annot_type") + " variants, ")
-#                    doc.append("where " + countVCF(
-#                        config=sample_config,
-#                        var_caller=var_caller,
-#                        flag="pathological"
-#                    ) + " of all predicted to have high functional impact.")
-#                    fmt = "X[r] X[r] X[r] X[r] X[r] X[r]"
-#                    with doc.create(LongTabu(fmt)) as data_table:
-#                        header_row1 = [
-#                            "Chrom", "Pos", "RefAllele", "AltAllele", "Type", "DP/AF"
-#                        ]
-#                        data_table.add_hline()
-#                        data_table.add_row(header_row1, mapper=[bold])
-#                        data_table.add_hline()
-#                        data_table.add_empty_row()
-#                        # data_table.end_table_header()
-#                        row = extractVCFstat()
-#                        for i in range(2):
-#                            data_table.add_row(row)
-#                        data_table.add_hline()
-
-#        fmt = "X[r] X[r] X[r] X[r] X[r] X[r]"
-#        with doc.create(LongTabu(fmt)) as data_table:
-#            header_row1 = [
-#                "Variant Caller", "Total", "Passed", "Misense", "Sense", "Patholigical"
-#            ]
-#            data_table.add_hline()
-#            data_table.add_row(header_row1, mapper=[bold])
-#            data_table.add_hline()
-#            data_table.add_empty_row()
-#            #data_table.add_caption("tmp")
-#            row = extractVCFstat()
-#            for var_caller in sample_config["vcf"]:
-#                row = varCallerSummary( config=sample_config, var_caller=var_caller)
-#                data_table.add_row(row)
-#            data_table.add_hline()
