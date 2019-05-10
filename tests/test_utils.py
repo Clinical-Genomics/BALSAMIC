ebc17cb46d3598b800255520bdb660050ca08fd5
imsarath <sarath.murugan@outlook.com>
2019-05-10 14:44:28 +0200

    style: added pep8 style to previously committed py files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,13 +2,14 @@ import unittest
 
 from BALSAMIC.tools import get_ref_path
 
+
 class TestUtils(unittest.TestCase):
-	"""docstring for TestUtils"""
-	def test_get_ref_path(self):
-		test_ref = "test_data/references/reference.json"
+    """docstring for TestUtils"""
 
-		test_ref_json = get_ref_path(test_ref)
+    def test_get_ref_path(self):
+        test_ref = "test_data/references/reference.json"
 
-		self.assertTrue(isinstance(test_ref_json, dict))
-		self.assertTrue(test_ref_json['path']['genomefa'].startswith('/'))
-		
+        test_ref_json = get_ref_path(test_ref)
+
+        self.assertTrue(isinstance(test_ref_json, dict))
+        self.assertTrue(test_ref_json['path']['genomefa'].startswith('/'))
