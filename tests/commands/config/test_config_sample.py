5b137124b309058329d02b847de9f7606d1f5617
imsarath <sarath.murugan@outlook.com>
2019-05-16 14:19:09 +0200

    test (cli): testcases added for cli and config-sample

---
+++ tests/commands/config/test_config_sample.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pytest
+
+from BALSAMIC.commands.config import get_config, write_json
+
+
+def iterdict(dic):
+    """ dictionary iteration - returns generator"""
+    for key, value in dic.items():
+        if isinstance(value, dict):
+            iterdict(value)
+        else:
+            yield key, value
+
+
+def test_get_config(config_files):
+    """ testing get config function"""
+    files = [
+        "install", "reference", "sample", "analysis_paired",
+        "analysis_paired_umi", "analysis_single", "analysis_single_umi"
+    ]
+
+    for file in files:
+        assert config_files[file] in get_config(file)
+
+
+def test_write_json(tmp_path, config_files):
+    """ tested by tmp_path fixture using reference.json data """
+    ref_json = json.load(open(config_files['reference'], 'r'))
+
+    tmp = tmp_path / "tmp"
+    tmp.mkdir()
+    output_json = tmp / "output.json"
+    write_json(ref_json, output_json)
+    output = output_json.read_text()
+
+    for key, value in iterdict(ref_json):
+        assert key in output
+        assert value in output
--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,16 @@ def cli_runner():
 @pytest.fixture
 def invoke_cli(cli_runner):
     return partial(cli_runner.invoke, cli)
+
+
+@pytest.fixture(scope='session')
+def config_files():
+    return {
+        "install": "BALSAMIC/config/install.json",
+        "reference": "BALSAMIC/config/reference.json",
+        "sample": "BALSAMIC/config/sample.json",
+        "analysis_paired": "BALSAMIC/config/analysis_paired.json",
+        "analysis_paired_umi": "BALSAMIC/config/analysis_paired_umi.json",
+        "analysis_single": "BALSAMIC/config/analysis_single.json",
+        "analysis_single_umi": "BALSAMIC/config/analysis_single_umi.json"
+    }
