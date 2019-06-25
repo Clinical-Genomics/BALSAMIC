1ee66e18e507b65a2e7a2e4e41d4652ff0846cf2
hassanfa <hassan.foroughi@gmail.com>
2019-06-25 13:08:28 +0200

    refactor: cli_utils -> cli

--- tests/tools/test_utils.py
+++ tests/tools/test_utils.py
@@ -1,8 +1,8 @@
 import json
 import pytest
 
-from BALSAMIC.utils.cli_utils import get_ref_path, iterdict
-from BALSAMIC.utils.cli_utils import SnakeMake, get_packages, get_package_split, get_snakefile
+from BALSAMIC.utils.cli import get_ref_path, iterdict
+from BALSAMIC.utils.cli import SnakeMake, get_packages, get_package_split, get_snakefile
 from BALSAMIC.utils.rule_utils import get_chrom, get_vcf, get_sample_type, get_conda_env, \
     get_picard_mrkdup
 
