5d3c6f03c907ce6e59da3b2c6fa185888bc460bf
imsarath <sarath.murugan@outlook.com>
2019-07-08 11:55:19 +0200

    feat (run): run-reference added for local run mode

--- BALSAMIC/commands/run/reference.py
+++ BALSAMIC/commands/run/reference.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python
 
+import json
+import subprocess
 import click
 
 from BALSAMIC.utils.cli import get_sbatchpy
 from BALSAMIC.utils.cli import get_snakefile, SnakeMake, get_config
 
 
-
 @click.command('reference', short_help="Run the GenerateRef workflow")
 @click.option('-s', "--snakefile", default=get_snakefile('generate_ref'),
               help="snakefile for reference generation")
 @click.option('-c', '--configfile', required=True, help="Config file to run the workflow")
-@click.option('--run-mode', default='slurm', type=click.Choice(["slurm", "local"]),
+@click.option('--run-mode', default='local', type=click.Choice(["slurm", "local"]),
               help="Run mode to use.(LOCAL, SLURM for HPC)")
 @click.option('--cluster-config', show_default=True, default=get_config('cluster'),
               type=click.Path(), help='SLURM config json file.')
@@ -26,7 +27,28 @@ from BALSAMIC.utils.cli import get_snakefile, SnakeMake, get_config
               help='Force run all analysis. This is same as snakemake --forceall')
 @click.option('--snakemake-opt', multiple=True, help='Pass these options directly to snakemake')
 @click.pass_context
-def reference(context, snakefile, run_mode, cluster_config, log_file, run_analysis, qos,
+def reference(context, snakefile, configfile, run_mode, cluster_config, log_file, run_analysis, qos,
               force_all, snakemake_opt):
     """ Run generate reference workflow """
     click.echo("Reference generation workflow started")
+
+    with open(configfile, "r") as config_fh:
+        config = json.load(config_fh)
+
+    # Construct snakemake command to run workflow
+    balsamic_run = SnakeMake()
+    balsamic_run.working_dir = config['output']
+    balsamic_run.snakefile = snakefile
+    balsamic_run.configfile = configfile
+    balsamic_run.run_mode = run_mode
+    balsamic_run.forceall = force_all
+    balsamic_run.run_analysis = run_analysis
+    balsamic_run.sm_opt = snakemake_opt
+    # balsamic_run.cluster_config = cluster_config
+    # balsamic_run.sbatch_py = get_sbatchpy()
+    # balsamic_run.log_path = logpath
+    # balsamic_run.script_path = scriptpath
+    # balsamic_run.result_path = resultpath
+    # balsamic_run.qos = qos
+
+    subprocess.run(balsamic_run.build_cmd(), shell=True)
