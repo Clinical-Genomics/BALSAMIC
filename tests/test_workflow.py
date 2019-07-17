3aa7a6f1f7f6ddcb67509bd7c21a2074e4ad72fb
imsarath <sarath.murugan@outlook.com>
2019-07-17 14:19:58 +0200

    test (workflow): workflow testcase included for qc, paired, single

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -6,15 +6,18 @@ import snakemake
 from BALSAMIC.utils.cli import get_snakefile, get_abs_path
 
 
-def test_variant_calling_paired(sample_config):
+def test_workflow(sample_config):
     # GIVEN a sample config dict and snakefile
-    snakefile = get_snakefile('single')
+    workflows = ['paired', 'qc', 'single']
     config_json = 'tests/test_data/config.json'
     rules = {'rule_directory': get_abs_path(config_json)}
 
-    # WHEN invoking snakemake module
-    assert snakemake.snakemake(snakefile,
-                               config=rules,
-                               configfile=config_json,
-                               dryrun=True)
+    # WHEN invoking snakemake module with dryrun option
+    # THEN it should return true
+    for workflow in workflows:
+        snakefile = get_snakefile(workflow)
+        assert snakemake.snakemake(snakefile,
+                                   config=rules,
+                                   configfile=config_json,
+                                   dryrun=True)
 
