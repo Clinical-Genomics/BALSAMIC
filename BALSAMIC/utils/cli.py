53dd7e60338d8a7b2327b0af5c66bb2103841239
hassanfa <hassan.foroughi@gmail.com>
2019-07-18 11:59:33 +0200

    feat: adds capture stdout class to handle snakemake graphdot

--- BALSAMIC/utils/cli.py
+++ BALSAMIC/utils/cli.py
@@ -1,10 +1,24 @@
 import os
 import re
 import json
+import yaml
+import sys
+from io import StringIO
 from pathlib import Path
 from itertools import chain
-import yaml
 
+class CaptureStdout(list):
+    '''
+    Captures stdout.
+    '''
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio    # free up some memory
+        sys.stdout = self._stdout
 
 class sbatch:
     '''
