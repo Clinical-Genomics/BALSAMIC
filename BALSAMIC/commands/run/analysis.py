f9be68a34c132525581edef1d1cc00566b75a831
Sarath Murugan <sarath.murugan@outlook.com>
2019-09-18 14:42:48 +0200

    Cli sentieon (#214)
    * feat: added wgs sentieon workflow into balsamic-cli
    
    * fix: travis env variables
    
    * fix: env removed from travis.yaml
    
    * fix: travis multiple env variable added in single build
    
    * fix: added default value for get_snakemakefile
    
    * refactor: split sv(manta) into separate rule - somatic_sv
    
    * fix: remove empty param
    
    * feat: add somatic sv for tumor only - wgs, targeted

--- BALSAMIC/commands/run/analysis.py
+++ BALSAMIC/commands/run/analysis.py
@@ -112,6 +112,7 @@ def analysis(context, snake_file, sample_config, run_mode, cluster_config,
     scriptpath = sample_config['analysis']['script']
     resultpath = sample_config['analysis']['result']
     case_name = sample_config['analysis']['case_id']
+    sequencing_type = sample_config['analysis']['sequencing_type']
 
     if run_analysis:
         # if not dry run, then create (new) log/script directory
@@ -131,11 +132,11 @@ def analysis(context, snake_file, sample_config, run_mode, cluster_config,
 
     # Construct snakemake command to run workflow
     balsamic_run = SnakeMake()
-    balsamic_run.case_name = case_name 
+    balsamic_run.case_name = case_name
     balsamic_run.working_dir = sample_config['analysis']['analysis_dir'] +  \
         case_name + '/BALSAMIC_run/'
     balsamic_run.snakefile = snake_file if snake_file else get_snakefile(
-        analysis_type)
+        analysis_type, sequencing_type)
     balsamic_run.configfile = sample_config_path
     balsamic_run.run_mode = run_mode
     balsamic_run.cluster_config = cluster_config
