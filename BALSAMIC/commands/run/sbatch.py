e03728b0b742f3c5504c419f4975d53ade31cdfa
imsarath <sarath.murugan@outlook.com>
2019-10-29 15:14:32 +0100

    feat: qsub scheduler added

--- BALSAMIC/commands/run/sbatch.py
+++ BALSAMIC/commands/run/sbatch.py
@@ -57,6 +57,62 @@ class SbatchScheduler:
         return 'sbatch' + ' ' + ' '.join(sbatch_options)
 
 
+class QsubScheduler(object):
+    """docstring for QsubScheduler"""
+    def __init__(self):
+        self.account = None
+        self.dependency = None
+        self.error = None
+        self.resources = None
+        self.mail_type = None
+        self.mail_user = None
+        self.ntasks = None
+        self.output = None
+        self.qos = None
+        self.script = None
+        self.time = None
+
+    def build_cmd(self):
+
+        resource_params = ""
+        qsub_options = list()
+
+        if self.ntasks:
+            resource_params += " -l procs={} ".format(str(self.ntasks))
+
+        if self.time:
+            resource_params += " -l walltime={} ".format(str(self.time))
+
+        if self.account:
+            qsub_options.append(" -A " + str(self.account))
+
+        if self.error:
+            qsub_options.append(" -e " + str(self.error))
+
+        if self.output:
+            qsub_options.append(" -o " + str(self.output))
+
+        if self.mail_type:
+            qsub_options.append(" -m " + str(self.mail_type))
+
+        if self.mail_user:
+            qsub_options.append(" -M " + str(self.mail_user))
+
+        if self.qos:
+            qsub_options.append(" -p " + str(self.qos))
+
+        if self.dependency:
+            qsub_options.append(" -W \"" + str(self.dependency) + "\"")
+
+        if self.script:
+            qsub_options.append(" {} ".format(self.script))
+
+        if resource_params:
+            qsub_options.extend(resource_params)
+
+        return "qsub " + " ".join(qsub_options)
+
+
 def read_sample_config(input_json):
     ''' load input sample_config file. Output of balsamic config sample. '''
 
@@ -150,13 +206,14 @@ def get_parser():
                         help="{{dependencies}} from snakemake")
     parser.add_argument("snakescript", help="Snakemake script")
     parser.add_argument("--sample-config",
-                        help="balsamic config sample output")
-    parser.add_argument('--slurm-account',
+                        help='balsamic config sample output')
+    parser.add_argument("--profile", help="profile to run jobs")
+    parser.add_argument("--account",
                         required=True,
-                        help='SLURM account name')
-    parser.add_argument('--slurm-qos', default='low', help='SLURM job QOS')
-    parser.add_argument('--slurm-mail-type', help='SLURM mail type')
-    parser.add_argument('--slurm-mail-user', help='SLURM mail user')
+                        help='cluster account name')
+    parser.add_argument("--priority", default='low', help='cluster job Priority (slurm - QOS)')
+    parser.add_argument("--mail-type", help='cluster mail type')
+    parser.add_argument("--mail-user", help='mail user')
     parser.add_argument("--log-dir", help="Log directory")
     parser.add_argument("--result-dir", help="Result directory")
     parser.add_argument("--script-dir", help="Script directory")
@@ -169,14 +226,17 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    sbatch_cmd = SbatchScheduler()
-
     jobscript = args.snakescript
     job_properties = read_job_properties(jobscript)
     shutil.copy2(jobscript, args.script_dir)
     jobscript = os.path.join(args.script_dir, os.path.basename(jobscript))
 
-    if not args.slurm_mail_type:
+    if args.profile == 'slurm':
+        scheduler_cmd = SbatchScheduler()
+    elif args.profile == 'qsub':
+        scheduler_cmd = QsubScheduler()
+
+    if not args.mail_type:
         mail_type = job_properties["cluster"]["mail_type"]
 
     sample_config = read_sample_config(input_json=args.sample_config)
@@ -194,23 +254,23 @@ def main():
     #                                        sbatch_script=sbatch_script)
     #        jobscript = sbatch_script
 
-    sbatch_cmd.account = args.slurm_account
-    sbatch_cmd.mail_type = mail_type
-    sbatch_cmd.error = os.path.join(args.log_dir,
+    scheduler_cmd.account = args.account
+    scheduler_cmd.mail_type = mail_type
+    scheduler_cmd.error = os.path.join(args.log_dir,
                                     os.path.basename(jobscript) + "_%j.err")
-    sbatch_cmd.output = os.path.join(args.log_dir,
+    scheduler_cmd.output = os.path.join(args.log_dir,
                                      os.path.basename(jobscript) + "_%j.out")
 
-    sbatch_cmd.ntasks = job_properties["cluster"]["n"]
-    sbatch_cmd.time = job_properties["cluster"]["time"]
-    sbatch_cmd.mail_user = args.slurm_mail_user
-    sbatch_cmd.script = jobscript
+    scheduler_cmd.ntasks = job_properties["cluster"]["n"]
+    scheduler_cmd.time = job_properties["cluster"]["time"]
+    scheduler_cmd.mail_user = args.mail_user
+    scheduler_cmd.script = jobscript
 
     if args.dependencies:
-        sbatch_cmd.dependency = ','.join(
+        scheduler_cmd.dependency = ','.join(
             ["afterok:%s" % d for d in args.dependencies])
 
-    jobid = submit_job(sbatch_cmd=sbatch_cmd.build_cmd())
+    jobid = submit_job(scheduler_cmd=scheduler_cmd.build_cmd())
 
     #    if balsamic_run_mode == 'container' and 'singularity' in sample_config:
     #        write_sbatch_dump(sbatch_file=sbatch_file,
