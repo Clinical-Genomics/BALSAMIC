a1fa44b67ce24ab3d49fc1ff4353d97bae747fd0
imsarath <sarath.murugan@outlook.com>
2019-11-06 14:54:11 +0100

    test: added for run analysis - click abort

--- tests/commands/run/test_run_analysis.py
+++ tests/commands/run/test_run_analysis.py
@@ -20,7 +20,16 @@ def test_run_analysis_tumor_only_dry_run(invoke_cli, tumor_only_config,
                                          tumor_normal_config):
     # GIVEN a tumor-only config file
     # WHEN running analysis
-    result = invoke_cli(['run', 'analysis', '-s', tumor_only_config])
+    result = invoke_cli(['run', 'analysis', '-s', tumor_only_config, '--mail-type', 'FAIL'])
 
     # THEN it should run without any error
     assert result.exit_code == 0
+
+
+def test_run_analysis_click_abort(invoke_cli, tumor_only_config, tumor_normal_config):
+    # GIVEN a tumor-only config file
+    # WHEN running analysis
+    result = invoke_cli(['run', 'analysis', '-r', '-s', tumor_only_config])
+
+    # THEN it should abort with error
+    assert result.exit_code == 1
