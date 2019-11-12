8f24a19449c0206bc920097ba9b8815623b8d161
hassanfa <hassan.foroughi@gmail.com>
2019-11-12 09:32:38 +0100

    remove mail to test

--- tests/commands/run/test_run_analysis.py
+++ tests/commands/run/test_run_analysis.py
@@ -21,7 +21,7 @@ def test_run_analysis_tumor_only_dry_run(invoke_cli, tumor_only_config,
                                          tumor_normal_config):
     # GIVEN a tumor-only config file
     # WHEN running analysis
-    result = invoke_cli(['run', 'analysis', '-s', tumor_only_config, '--mail-type', 'FAIL'])
+    result = invoke_cli(['run', 'analysis', '-s', tumor_only_config])
 
     # THEN it should run without any error
     assert result.exit_code == 0
@@ -36,23 +36,23 @@ def test_run_analysis_click_abort(invoke_cli, tumor_only_config, tumor_normal_co
     assert result.exit_code == 1
 
 
-#def test_run_analysis_create_dir(invoke_cli, tumor_only_config, tmp_path):
-#    # GIVEN a tumor-only config file
-#    # WHEN running analysis
-#    with open(tumor_only_config) as fh:
-#        tumor_config = json.load(fh)
-#    log_dir = tumor_config['analysis']['log']
-#
-#    with mock.patch.object(subprocess, 'run') as mocked:
-#        mocked.return_value.stdout = 1
-#        log_file = os.path.join(log_dir, 'log.file')
-#        
-#        result = invoke_cli(['run', 'analysis', '-s', tumor_only_config, '-r', '--account',
-#                             'development'])
-#        
-#        # THEN it should abort with error
-#        #assert os.path.exists(log_dir)
-#        assert os.path.exists(re.sub('/$', '.1/', log_dir))
+def test_run_analysis_create_dir(invoke_cli, tumor_only_config, tmp_path):
+    # GIVEN a tumor-only config file
+    # WHEN running analysis
+    with open(tumor_only_config) as fh:
+        tumor_config = json.load(fh)
+    log_dir = tumor_config['analysis']['log']
+
+    with mock.patch.object(subprocess, 'run') as mocked:
+        mocked.return_value.stdout = 1
+        log_file = os.path.join(log_dir, 'log.file')
+        
+        result = invoke_cli(['run', 'analysis', '-s', tumor_only_config, '-r', '--account',
+                             'development'])
+        
+        # THEN it should abort with error
+        #assert os.path.exists(log_dir)
+        assert os.path.exists(re.sub('/$', '.1/', log_dir))
 
 
 # def test_run_analysis_exception(invoke_cli, tumor_only_config):
