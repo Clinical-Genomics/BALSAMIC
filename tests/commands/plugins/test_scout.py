311defeda7b489696abf614025f5d1f678659a19
hassanfa <hassan.foroughi@gmail.com>
2019-12-07 12:30:17 +0100

    refactor scout config generation

--- tests/commands/plugins/test_scout.py
+++ tests/commands/plugins/test_scout.py
@@ -3,10 +3,20 @@ import pytest
 import BALSAMIC
 from BALSAMIC.commands.base import cli
 
-def test_scout(invoke_cli, tumor_normal_config):
+def test_scout_tumor_normal(invoke_cli, tumor_normal_config):
     # GIVEN a tumor-normal config file
     # WHEN running analysis
-    result = invoke_cli(['plugins', 'scout', '--sample-config', tumor_normal_config])
+    result = invoke_cli(['plugins', 'scout', '--sample-config', tumor_normal_config, '--customer-id', 'cust000'])
 
     # THEN it should run without any error
+    print(result)
+    assert result.exit_code == 0
+
+def test_scout_tumor_only(invoke_cli, tumor_only_config):
+    # GIVEN a tumor-only config file
+    # WHEN running analysis
+    result = invoke_cli(['plugins', 'scout', '--sample-config', tumor_only_config, '--customer-id', 'cust000'])
+
+    # THEN it should run without any error
+    print(result)
     assert result.exit_code == 0
