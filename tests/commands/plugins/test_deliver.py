94b8e0636eeaba0ec762fe50facea8d49f2dae63
Hassan Foroughi <hassanfa@users.noreply.github.com>
2020-03-23 11:30:13 +0100

    Refactor delivery (#291)
    * Add delivery rule
    
    * bgzip force zip if file exists
    
    * force tabix index creation
    
    * Add html and json to multiqc
    
    * delivery rule
    
    * a bit stuff on delivery thingies
    
    * changed sample wildcard to case_name2
    
    * Added functions for delivery raw output
    
    * delivery raw output
    
    * exceptions
    
    * refactor delivery
    
    * color class
    
    * some comments deleted
    
    * Add tumor_normal to delivery test
    
    * resolves missing wildcard_value in not-finished analysis
    
    * move functions in to utils
    
    * Moved utils
    
    * correct import
    
    * move exception out2
    
    * correct raise message
    
    * deepcopy it
    
    * simplified logic
    
    * double extensions
    
    * simplfy futher
    
    * general method for processing delivery dict
    
    * simplification continues
    
    * create dir even if it exists
    
    * merge conditions

--- tests/commands/plugins/test_deliver.py
+++ tests/commands/plugins/test_deliver.py
@@ -10,3 +10,12 @@ def test_deliver(invoke_cli, tumor_only_config):
 
     # THEN it should run without any error
     assert result.exit_code == 0
+
+
+def test_deliver(invoke_cli, tumor_normal_config):
+    # GIVEN a tumor-normal config file
+    # WHEN running analysis
+    result = invoke_cli(['plugins', 'deliver', '--sample-config', tumor_normal_config])
+
+    # THEN it should run without any error
+    assert result.exit_code == 0
