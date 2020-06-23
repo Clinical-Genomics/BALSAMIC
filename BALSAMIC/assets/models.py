06984071272a093626d0e05d7369588ce6bebdaa
hassanfa <hassan.foroughi@gmail.com>
2020-06-23 20:11:12 +0200

    docstring and some formatting

--- BALSAMIC/assets/models.py
+++ BALSAMIC/assets/models.py
@@ -1,35 +1,66 @@
 '''
 Contains constants and models for analysis or filtering
 '''
-from pydantic import BaseModel
 from typing import Optional
+from pydantic import BaseModel
 
-class NgsFilters(BaseModel):
-    value: float
-    name: str
-    tag: str
-    
+
+class VCFAttributes(BaseModel):
+    """General purpose filter to manage various VCF attributes
+
+    This class handles three parameters for the purpose filtering variants
+    based on a tag_values, filter_name, and which field in VCF.
+
+    E.g. AD=VCFAttributes(tag_value=5, filter_name="balsamic_low_tumor_ad", field="INFO")
+    A value of 5 from INFO field and filter_name will be balsamic_low_tumor_ad
+
+    Attributes:
+      tag_value: float
+      filter_name: str
+      field: str
+    """
+
+    tag_value: float
+    filter_name: str
+    field: str
+
+
 class VarCallerFilter(BaseModel):
-    AD: NgsFilters
-    AF_min: Optional[NgsFilters]
-    AF_max: Optional[NgsFilters]
-    MQ: Optional[NgsFilters]
-    DP: NgsFilters
+    """General purpose for variant caller filters
+
+    This class handles attributes and filter for variant callers
+
+    Attributes:
+      AD: VCFAttributes (required); minimum allelic depth
+      AF_min: VCFAttributes (optional); minimum allelic fraction
+      AF_max: VCFAttributes (optional); maximum allelic fraction
+      MQ: VCFAttributes (optional); minimum mapping quality
+      DP: VCFAttributes (optional); minimum read depth
+      varcaller_name: str (required); variant caller name
+      filter_type: str (required); filter name for variant caller
+      analysis_type: str (required); analysis type e.g. tumor_normal or tumor_only
+      description: str (required); comment section for description
+    """
+
+    AD: VCFAttributes
+    AF_min: Optional[VCFAttributes]
+    AF_max: Optional[VCFAttributes]
+    MQ: Optional[VCFAttributes]
+    DP: VCFAttributes
     filter_version: str
     varcaller_name: str
     filter_type: str
     analysis_type: str
     description: str
 
+
 VARDICT = VarCallerFilter(
-    AD=NgsFilters(value=5, name="balsamic_low_tumor_ad", tag="INFO"),
-    DP=NgsFilters(value=100, name="balsamic_low_tumor_dp", tag="INFO"),
-    MQ=NgsFilters(value=50, name="balsamic_low_mq", tag="INFO"),
-    AF_max=NgsFilters(value=1, name="balsamic_af_one", tag="INFO"),
-    AF_min=NgsFilters(value=0.02, name="balsamic_low_af", tag="INFO"),
-    filter_version = "0.0.1",
-    varcaller_name = "VarDict",
+    AD=VCFAttributes(value=5, name="balsamic_low_tumor_ad", tag="INFO"),
+    DP=VCFAttributes(value=100, name="balsamic_low_tumor_dp", tag="INFO"),
+    MQ=VCFAttributes(value=50, name="balsamic_low_mq", tag="INFO"),
+    AF_max=VCFAttributes(value=1, name="balsamic_af_one", tag="INFO"),
+    AF_min=VCFAttributes(value=0.02, name="balsamic_low_af", tag="INFO"),
+    varcaller_name="VarDict",
     filter_type="general",
     analysis_type="tumor_only",
-    description="General purpose filters used for filtering VarDict"
-)
+    description="General purpose filters used for filtering VarDict")
