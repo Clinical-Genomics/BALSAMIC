e5cc2fbd06b2f00317e3df0df09734983fbfb85d
hassanfa <hassan.foroughi@gmail.com>
2020-06-23 19:45:35 +0200

    feat, add pydantic for ngsfilter settings

--- BALSAMIC/assets/models.py
+++ BALSAMIC/assets/models.py
@@ -1,33 +1,35 @@
 '''
 Contains constants and models for analysis or filtering
 '''
+from pydantic import BaseModel
+from typing import Optional
 
-# A dictionary of various filters for variant callers
-# 'filter': the most basic filters of them all
-# 'exclude_filter': exclude criteria for variants
-# 'somatic_filter': a more sophisticated filter to filter out possible non somatic filters
-NGS_FILTER = {
-    'tumor_only': {
-        'vardict': {
-            'general': {
-                'AD': ['20', 'balsamic_low_tumor_ad'],
-                'MQ': ['50', 'balsamic_low_mq'],
-                'DP': ['100', 'balsamic_low_tumor_dp'],
-                'AF_min': ['0.05', 'balsamic_low_af'],
-                'AF_max': ['1', 'balsamic_af_equal_one']
-            }
-        }
-    },
-    'tumor_normal': {
-        'vardict': {
-            'general': {
-                'AD': ['20', 'balsamic_low_tumor_ad'],
-                'MQ': ['50', 'balsamic_low_mq'],
-                'DP': ['100', 'balsamic_low_tumor_dp'],
-                'AF_min': ['0.05', 'balsamic_low_af'],
-                'AF_max': ['1', 'balsamic_af_equal_one']
-                'TN_af_ration': ['5', 'balsamic_low_tn_af_ratio']
-            }
-        }
-    }
-}
+class NgsFilters(BaseModel):
+    value: float
+    name: str
+    tag: str
+    
+class VarCallerFilter(BaseModel):
+    AD: NgsFilters
+    AF_min: Optional[NgsFilters]
+    AF_max: Optional[NgsFilters]
+    MQ: Optional[NgsFilters]
+    DP: NgsFilters
+    filter_version: str
+    varcaller_name: str
+    filter_type: str
+    analysis_type: str
+    description: str
+
+VARDICT = VarCallerFilter(
+    AD=NgsFilters(value=5, name="balsamic_low_tumor_ad", tag="INFO"),
+    DP=NgsFilters(value=100, name="balsamic_low_tumor_dp", tag="INFO"),
+    MQ=NgsFilters(value=50, name="balsamic_low_mq", tag="INFO"),
+    AF_max=NgsFilters(value=1, name="balsamic_af_one", tag="INFO"),
+    AF_min=NgsFilters(value=0.02, name="balsamic_low_af", tag="INFO"),
+    filter_version = "0.0.1",
+    varcaller_name = "VarDict",
+    filter_type="general",
+    analysis_type="tumor_only",
+    description="General purpose filters used for filtering VarDict"
+)
