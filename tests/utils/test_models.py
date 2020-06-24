aca3d7b60f6e52acda1f286a993c5354891a8921
hassanfa <hassan.foroughi@gmail.com>
2020-06-24 15:18:02 +0200

    tests, add tests for models

---
+++ tests/utils/test_models.py
@@ -0,0 +1,52 @@
+import pytest
+
+from BALSAMIC.utils.models import (VCFAttributes, VarCallerFilter, VARDICT)
+
+
+def test_VCFAttributes():
+    """test VCFAttributes model for correct validation"""
+
+    # GIVEN a VCF attribute
+    dummy_attribute = {
+        "tag_value": 5.0,
+        "filter_name": "dummy_filter_name",
+        "field": "INFO"
+    }
+
+    # WHEN building the model
+    dummy_attribute_built = VCFAttributes(**dummy_attribute)
+
+    # THEN assert values can be reterived currently
+    assert dummy_attribute_built.tag_value == 5.0
+    assert dummy_attribute_built.field == "INFO"
+    assert dummy_attribute_built.filter_name == "dummy_filter_name"
+
+
+def test_VarCallerFilter():
+    """test required VarCallerFilters for being set correctly"""
+
+    # GIVEN a VarCallerFilter
+    dummy_varcaller = {
+        "AD": {
+            "tag_value": 5.0,
+            "filter_name": "dummy_alt_depth",
+            "field": "INFO"
+        },
+        "DP": {
+            "tag_value": 100.0,
+            "filter_name": "dummy_depth",
+            "field": "INFO"
+        },
+        "varcaller_name": "dummy_varcaller",
+        "filter_type": "dummy_ffpe_filter",
+        "analysis_type": "dummy_tumor_only",
+        "description": "dummy description of this filter"
+    }
+
+    # WHEN building the model
+    dummy_varcaller_filter = VarCallerFilter(**dummy_varcaller)
+
+    # THEN assert required values are set
+    assert dummy_varcaller_filter.AD.tag_value == 5.0
+    assert dummy_varcaller_filter.DP.tag_value == 100.0
+    assert dummy_varcaller_filter.analysis_type == "dummy_tumor_only"
