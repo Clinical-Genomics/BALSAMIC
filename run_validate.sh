2b3d64f40a7851844997052fd1e42f72d88d94cc
hassanfa <hassan.foroughi@gmail.com>
2020-06-26 16:52:59 +0200

    add run/dryrun, and all option2

--- run_validate.sh
+++ run_validate.sh
@@ -11,17 +11,18 @@ _nocol='\033[0m';
 
 function usage() {
   echo $"
-USAGE: [ -a <T|TN> -c _condaenv -m <run|config|all>  -t <panel|WGS> ]
+USAGE: [ -a <T|TN> -c _condaenv -m <run|config|all> -t <panel|WGS> -r ]
   
   -a [required] T: Tumor only, TN: tumor normal
   -c Conda environment where BALSAMIC is installed. If not specified, it will use current environment.
   -m [required] config: only create config file, run: create config file and start analysis
   -t [required] panel: target sequencing workflow (also includes WES), WGS: whole genome sequencing workflow
+  -r Flag. Set to submit jobs instead of running in dry mode
   -h Show this help and exit
 " 
 }
 
-while getopts ":a:m:c:t:" opt; do
+while getopts ":a:m:c:t:r" opt; do
   case ${opt} in
     a)
       _analysis=${OPTARG}
@@ -35,12 +36,15 @@ while getopts ":a:m:c:t:" opt; do
     m)
       _startmode=${OPTARG}
       echo "start mode set to" "${OPTARG}"
-      [[ $_startmode == 'config' || $_startmode == 'run' ]] || ( usage >&2; exit 1)
+      [[ $_startmode == 'config' || $_startmode == 'run' || $_startmode == 'all' ]] || ( usage >&2; exit 1)
       ;;
     t)
       _ngstype=${OPTARG}
       echo "workflow set to " "${OPTARG}"
       [[ $_ngstype == 'panel' || $_ngstype == 'WGS' ]] || ( usage >&2; exit 1)
+      ;;
+    r)
+      rFlag=true;
       ;;
     *) echo "Invalid option: -${OPTARG}" >&2; usage >&2; exit 1;;
   esac
@@ -69,6 +73,10 @@ _tumor_fastq='tests/test_data/fastq/S1_R_1.fastq.gz'
 _normal_fastq='tests/test_data/fastq/S2_R_1.fastq.gz'
 _analysis_config='run_tests/'${_analysis}_${_ngstype}'/balsamic_config.json'
 
+if [[ ! -z ${rFlag} ]]; then
+  _run_analysis="-r"
+fi
+
 function balsamic_config() {
   balsamic config case \
     -t ${_tumor_fastq} \
@@ -84,11 +92,14 @@ function balsamic_config() {
 balsamic_run() {
   balsamic run analysis \
     -s ${_analysis_config} \
-    --account development -r
+    --account development ${_run_analysis}
 }
 
 if [[ $_startmode == 'config' ]]; then
   balsamic_config
+elif [[ $_startmode == 'run' ]]; then
+  balsamic_run
 else
+  balsamic_config
   balsamic_run
 fi
