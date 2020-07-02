e16958bb7a7a11f76655540e80b482be865d18c4
Mropat <mariaropat@gmail.com>
2020-07-02 12:20:44 +0200

    formatting

--- BALSAMIC/commands/run/analysis.py
+++ BALSAMIC/commands/run/analysis.py
@@ -147,7 +147,7 @@ def analysis(context, snake_file, sample_config, run_mode, cluster_config,
     # Construct snakemake command to run workflow
     balsamic_run = SnakeMake()
     balsamic_run.case_name = case_name
-    balsamic_run.working_dir = sample_config['analysis']['analysis_dir'] +  \
+    balsamic_run.working_dir = sample_config['analysis']['analysis_dir'] + "/" + \
         case_name + '/BALSAMIC_run/'
     balsamic_run.snakefile = snake_file if snake_file else get_snakefile(
         analysis_type, sequencing_type)
