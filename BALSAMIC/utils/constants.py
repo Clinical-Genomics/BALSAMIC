721cbe09803d15aa4a8dfe6263df4b7b951f0ce2
Mropat <mariaropat@gmail.com>
2020-07-02 16:47:22 +0200

    comments

--- BALSAMIC/utils/constants.py
+++ BALSAMIC/utils/constants.py
@@ -3,13 +3,22 @@ import sys
 
 from pathlib import Path
 
+"""This file contains consatant variables used during BALSAMIC config generation"""
+
+
+"""Path to conda folder containing YAML files with verions of software usen un BALSAMIC workflow"""
 CONDA_ENV_PATH = Path(
     Path(sys.modules["BALSAMIC"].__file__).parent.resolve() /
     "conda").as_posix()
+
+
+"""Path to config YAML file to be accessed by Snakemake"""
 
 CONDA_ENV_YAML = Path(
     Path(sys.modules["BALSAMIC"].__file__).parent.resolve() / "config" /
     "balsamic_env.yaml").as_posix()
 
+
+""""Path to rule files to be accessed by Snakemake"""
 RULE_DIRECTORY = Path(
     sys.modules["BALSAMIC"].__file__).parent.resolve().as_posix() + "/"
