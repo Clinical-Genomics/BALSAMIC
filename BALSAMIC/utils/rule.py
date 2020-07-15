d6032d639f97821643d9cea11ce65b11972fe5da
hassanfa <hassan.foroughi@gmail.com>
2020-07-15 16:06:00 +0200

    refactor, move workflow delivery functions to utils

--- BALSAMIC/utils/rule.py
+++ BALSAMIC/utils/rule.py
@@ -1,6 +1,8 @@
 import os
+import re
 import yaml
 from pathlib import Path
+import snakemake
 
 
 def get_chrom(panelfile):
@@ -106,3 +108,64 @@ def get_threads(cluster_config, rule_name='__default__'):
     """
 
     return cluster_config[rule_name]['n'] if rule_name in cluster_config else 8
+
+
+def get_rule_output(rules, rule_names=[]):
+    """get list of existing output files from a given workflow
+
+    Args:
+        rule_names: a list of rule names in the workflow. If no rules are given,
+          then it will get all rules from the workflow.
+        rules: snakemake rules object
+    
+    Returns:
+        output_files: list of tuples (file_name, rule_name, wildcard) for rule_names
+    """
+#    with open('/home/hassan.foroughi/repos/BALSAMIC/run_tests/TN_panel/get_rule_output.log', 'w') as f:
+#        print(rules, file = f)
+    output_files = list()
+    output_files.append(('output_file', 'rulename', 'wildcard_value'))
+    if not rule_names:
+        rule_names = vars(rules).keys()
+    for my_rule in rule_names:
+        for my_file in getattr(rules, my_rule).output:
+            for file_wildcard_list in snakemake.utils.listfiles(my_file):
+                output_files.append((file_wildcard_list[0], my_rule, list(file_wildcard_list[1])))
+    
+    return output_files
+
+
+def get_rule_output_raw(rules, rule_names=[], output_file_wildcards={}):
+    """get list of all possible output files from a given workflow
+
+    Args:
+        rule_names: a list of rule names in the workflow. If no rules are given,
+          then it will get all rules from the workflow.
+        rules: snakemake rules object
+        output_file_wildcards: a dictionary with wildcards as keys and values as list of wildcard values 
+    
+    Returns:
+        output_files: list of tuples (file_name, rule_name, wildcard) for rule_names
+    """
+    
+    output_files_raw = list()
+    output_files_raw.append(('output_file', 'rulename', 'wildcard_name'))
+    wildcard_sets = set()
+    if not rule_names:
+        rule_names = vars(rules).keys()
+    for my_rule in vars(rules).keys():
+        if my_rule in rule_names:
+            for my_file in getattr(rules, my_rule).output:
+                pattern = re.compile(r"{([^}\.[!:]+)")
+                wildcard_subset = dict()
+                if pattern.findall(my_file):
+
+                    wildcard_sets.update(pattern.findall(my_file))
+                    for w in pattern.findall(my_file):
+                        wildcard_subset[w] = output_file_wildcards[w]
+
+                    for my_file_expanded in snakemake.io.expand(my_file,**output_file_wildcards):
+                        output_files_raw.append((my_file_expanded, my_rule, list(wildcard_subset.keys())))
+                else:
+                    output_files_raw.append((my_file, my_rule, [])) 
+    return output_files_raw
