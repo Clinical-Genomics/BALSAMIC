e41937ab028315545f61579cb0618d2d00c1105a
hassanfa <hassan.foroughi@gmail.com>
2020-07-26 14:42:13 +0200

    refactor, cleanup delivery

--- BALSAMIC/utils/rule.py
+++ BALSAMIC/utils/rule.py
@@ -153,33 +153,3 @@ def get_rule_output(rules, rule_names: list, output_file_wildcards={}):
                 output_files.append((file_to_store, file_path_index, my_rule, ",".join(tags), ",".join(delivery_id["scope"]), file_format))
     
     return output_files
-
-
-def get_rule_output_raw(rules, output_file_wildcards={}):
-    """get list of all possible output files from a given workflow
-
-    Args:
-        rules: snakemake rules object
-        output_file_wildcards: a dictionary with wildcards as keys and values as list of wildcard values 
-    
-    Returns:
-        output_files: list of tuples (file_name, rule_name, wildcard) for rules
-    """
-    
-    output_files_raw = list()
-    output_files_raw.append(('output_file', 'rulename', 'wildcard_name'))
-    wildcard_sets = set()
-    for my_rule in vars(rules).keys():
-        for my_file in getattr(rules, my_rule).output:
-            pattern = re.compile(r"{([^}\.[!:]+)")
-            wildcard_subset = dict()
-            if pattern.findall(my_file):
-                wildcard_sets.update(pattern.findall(my_file))
-                for w in pattern.findall(my_file):
-                    wildcard_subset[w] = output_file_wildcards[w]
-
-                for my_file_expanded in snakemake.io.expand(my_file,**output_file_wildcards):
-                    output_files_raw.append((my_file_expanded, my_rule, list(wildcard_subset.keys())))
-            else:
-                output_files_raw.append((my_file, my_rule, [])) 
-    return output_files_raw
