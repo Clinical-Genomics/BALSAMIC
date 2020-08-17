ff22e5d79fee55bc8b933d858275f5263ce9ed73
Hassan Foroughi <hassanfa@users.noreply.github.com>
2020-08-17 13:54:07 +0200

    Refactor reference generate workflow (#371)
    * feat, placeholder for hg38
    
    * feat, a crude model for reference
    
    * feat, add support for genome version
    
    * refactor, add output_file and get_type properpty. some cleanup
    
    * refactor, add md5, remove cosmic rule
    
    * removed, unused workflow
    
    * remove print statement
    
    * tests, test only and only run, proper Given When Then
    
    * refactor, move reference model into models.py
    
    * refactor format
    
    * use meta model and get_output_file
    
    * constant path for hg19 file and remote file
    
    * create ref meta class
    
    * refactor, basedir and validate path
    
    * add basedir to reference dict
    
    * fix, output_file fixed for md5
    
    * output file set propoerly
    
    * refactor, prepare refgene rule
    
    * refactor, add refgene.sql output
    
    * proper return for nested if
    
    * refactor, add docstring and optional
    
    * add refseq
    
    * tests and check for no output file error
    
    * proper attribselect
    
    * test, fill dummy file with 8kb of random binary
    
    * formatter
    
    * test, add test for ref meta
    
    * test, assert text in exception
    
    * remove trailing white space
    
    * review comment addressed
    
    * refactor, exception for response

--- tests/commands/run/test_run_reference.py
+++ tests/commands/run/test_run_reference.py
@@ -12,7 +12,6 @@ def test_run_reference(invoke_cli, tmp_path, singularity_container):
     test_new_dir = tmp_path / "test_reference_dir_with_run"
     test_new_dir.mkdir()
 
-    # WHEN creating config.json in reference dir
     test_output_reference_config = test_new_dir / "config.json"
     test_output_reference_pdf = test_new_dir / "generate_ref_worflow_graph.pdf"
 
@@ -22,12 +21,10 @@ def test_run_reference(invoke_cli, tmp_path, singularity_container):
         str(test_new_dir)
     ])
 
+    # WHEN creating config.json in reference dir
     result_run = invoke_cli(
         ['run', 'reference', '-c',
          str(test_output_reference_config)])
 
     # THEN output config, pdf file generation, and reference dry run command exit code 0
-    assert result_config.exit_code == 0
     assert result_run.exit_code == 0
-    assert Path(test_output_reference_pdf).exists()
-    assert Path(test_output_reference_config).exists()
