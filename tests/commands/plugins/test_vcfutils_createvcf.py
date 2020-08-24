cf851888cf1b5dc6bb20f6df7b863ca673c3aa2c
ashwini06 <ashwini.jeggari@scilifelab.se>
2020-08-24 09:48:22 +0200

    ensids unit test

--- tests/commands/plugins/test_vcfutils_createvcf.py
+++ tests/commands/plugins/test_vcfutils_createvcf.py
@@ -1,6 +1,8 @@
 from datetime import date
 
 from BALSAMIC.commands.plugins.vcfutils import vcfheader
+from BALSAMIC.commands.plugins.vcfutils import collect_vcf_info
+import re
 
 def test_vcfheader_return_string():
     """test vcfheader for properly returning a VCF header"""
@@ -13,3 +15,16 @@ def test_vcfheader_return_string():
 
     #THEN it should return a VCF header with a valid current date
     assert valid_date_in_vcf in built_vcf_header
+
+
+def test_ensids_return_string():
+    """ test ensembl ids in a reference vcf file """
+    #GIVEN the ensembl ID
+    info = 'GENE=AKT1_ENST00000555528'
+    valid_ens_id = re.sub(r'(.*)(_ENST\d+)', r'\1', info)
+
+    #WHEN substitute the ensembl_ids
+    ens_id = collect_vcf_info(valid_ens_id)
+
+    #THEN it should return a valid matchi
+    assert valid_ens_id in ens_id
