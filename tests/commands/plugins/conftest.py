f4fbf92f08d2f6b0f858ed5874dfb9ab0acc55d6
ashwini06 <ashwini.jeggari@scilifelab.se>
2020-09-01 13:43:22 +0200

    fixed file paths from tests/

--- tests/commands/plugins/conftest.py
+++ tests/commands/plugins/conftest.py
@@ -2,12 +2,12 @@ import pytest
 
 @pytest.fixture
 def input_file():
-    return "test_data/vcf_tables/test_input.txt"
+    return "tests/test_data/vcf_tables/test_input.txt"
 
 @pytest.fixture
 def output_file():
-    return "test_data/vcf_tables/test_createVCF_output.vcf.gz"
+    return "tests/test_data/vcf_tables/test_createVCF_output.vcf.gz"
 
 @pytest.fixture
 def reference_file():
-    return "test_data/vcf_tables/test_reference.vcf.gz"
+    return "tests/test_data/vcf_tables/test_reference.vcf.gz"
