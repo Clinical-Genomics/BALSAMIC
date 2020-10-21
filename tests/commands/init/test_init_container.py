ff7919c14b2ac6feee80e3b3ffc821305edf3850
hassanfa <hassan.foroughi@gmail.com>
2020-10-21 16:51:27 +0200

    test for init container

---
+++ tests/commands/init/test_init_container.py
@@ -0,0 +1,12 @@
+def test_init_container(invoke_cli, tmp_path):
+    # Given a dummy path
+    test_new_dir = tmp_path / "test_container_dir"
+    test_new_dir.mkdir()
+
+    # WHEN creating config.json in reference dir
+    result = invoke_cli(
+        ['init', 'container', '--dry', '-o',
+         str(test_new_dir)])
+
+    # THEN output config and pdf file generate and command exit code 0
+    assert result.exit_code == 0
