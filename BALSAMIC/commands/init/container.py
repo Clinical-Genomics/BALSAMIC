8cfd203fcd9fe69c82345e7430ef640891ea3337
hassanfa <hassan.foroughi@gmail.com>
2020-10-22 20:37:42 +0200

    move cmd out, change to LOG.error

--- BALSAMIC/commands/init/container.py
+++ BALSAMIC/commands/init/container.py
@@ -61,21 +61,19 @@ def container(context, container_version, out_dir, force, dry):
     LOG.info("Image will be downloaded to {}".format(image_name))
     LOG.info("Starting download. This process can take some time...")
 
+    cmd = ["singularity", "pull", "--name", f"{image_name}"]
+    if force:
+        cmd.append("--force")
+    cmd.append(container_stub_url)
+
     try:
-        cmd = ["singularity", "pull", "--name", f"{image_name}"]
-
-        if force:
-            cmd.append("--force")
-
-        cmd.append(container_stub_url)
-
         if dry:
             LOG.info("Dry run mode, The following command will run: {}".format(
                 " ".join(cmd)))
         else:
             subprocess.check_output(cmd, cwd=out_dir, stderr=subprocess.STDOUT)
 
-    except subprocess.CalledProcessError as e:
-        raise BalsamicError("Failed to pull singularity image "
-                            "from {}:\n{}".format(container_stub_url,
-                                                  e.stdout.decode()))
+    except:
+        LOG.error("Failed to pull singularity image "
+                            "from {}".format(container_stub_url)) 
+        raise click.Abort()
