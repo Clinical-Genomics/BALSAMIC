575fd374ba1b2b9ed9c1f17f0a65eb917cf6ba2e
Keyvan Elhami <keyvan.elhami@scilifelab.se>
2020-10-28 13:06:47 +0100

    edited test_check_qc_criteria_output_csv_and_qc() to test three functions to increase cov

--- tests/utils/test_qc_check.py
+++ tests/utils/test_qc_check.py
@@ -1,6 +1,5 @@
-from BALSAMIC.utils.qc_check import read_hs_metrics, read_qc_table, check_qc_criteria
-from BALSAMIC.utils.qc_check import get_bait_name, get_sample_name, get_qc_criteria
-from BALSAMIC.utils.qc_check import write_output
+from BALSAMIC.utils.qc_check import read_hs_metrics, read_qc_table, check_qc_criteria, write_output
+from BALSAMIC.utils.qc_check import get_bait_name, get_sample_name, get_qc_criteria, failed_qc
 from BALSAMIC.utils.constants import HSMETRICS_QC_CHECK
 import os
 
@@ -59,7 +58,8 @@ def test_get_qc_criteria():
     nr_of_columns = list(criteria_df.columns)
     assert len(nr_of_columns) == 2, "number of columns != 2"
 
-def test_check_qc_criteria_and_output_csv():
+
+def test_check_qc_criteria_output_csv_and_qc():
     # GIVEN following variables
     config_file = "tests/test_data/qc_files/config_paired.json"
     hs_metrics = "tests/test_data/qc_files/multiqc_picard_HsMetrics.json"
@@ -68,15 +68,14 @@ def test_check_qc_criteria_and_output_csv():
     hs_metrics_df = read_hs_metrics(hs_metrics)
     sample_names = get_sample_name(config_file)
 
-    # WHEN calling the function
+    # WHEN calling the functions
     extract_criteria = check_qc_criteria(qc_criteria_df, hs_metrics_df,
                                          sample_names[0], sample_names[1])
+    write_output(extract_criteria, output_path)
+    qc_check = failed_qc(extract_criteria, sample_names[0], sample_names[1])
 
-    # THEN check if the output df has 3 indexes and if csv-file exists
+    # THEN check if the output df has 3 indexes, if csv-file exists and if qc check is string
     nr_of_indexes = list(extract_criteria.index)
     assert len(nr_of_indexes) == 3, "number of indexes != 3"
-
-    write_output(extract_criteria, output_path)
     assert os.path.isfile(output_path), "File doesn't exists"
-
-
+    assert isinstance(qc_check, str), "qc_check not string"
