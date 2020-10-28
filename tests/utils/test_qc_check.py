6f9ae7b661390cb36edec0246f05e1d5c3827400
Keyvan Elhami <keyvan.elhami@scilifelab.se>
2020-10-28 11:40:25 +0100

    merged two test func to one, added extra test to test_check_qc_criteria_and_output_csv():

--- tests/utils/test_qc_check.py
+++ tests/utils/test_qc_check.py
@@ -1,6 +1,8 @@
 from BALSAMIC.utils.qc_check import read_hs_metrics, read_qc_table, check_qc_criteria
 from BALSAMIC.utils.qc_check import get_bait_name, get_sample_name, get_qc_criteria
+from BALSAMIC.utils.qc_check import write_output
 from BALSAMIC.utils.constants import HSMETRICS_QC_CHECK
+import os
 
 
 def test_read_hs_metrics():
@@ -32,25 +34,16 @@ def test_read_qc_table():
         assert bol_list[n], "No values exists"
 
 
-def test_get_bait_name():
+def test_get_bait_and_sample_name():
     # GIVEN the file exists
     config_file = "tests/test_data/qc_files/config_paired.json"
 
     # WHEN reading the file
     bed = get_bait_name(config_file)
-
-    # THEN check if bed is string format
-    assert isinstance(bed, str), "bed is not in string format"
-
-
-def test_get_sample_name():
-    # GIVEN the file exists
-    config_file = "tests/test_data/qc_files/config_paired.json"
-
-    # WHEN reading the file
     sample_name = get_sample_name(config_file)
 
-    # THEN check if file name exsists
+    # THEN check if bed is string format and if sample name exists
+    assert isinstance(bed, str), "bed is not in string format"
     assert sample_name[0], "sample name doesn't exist"
 
 
@@ -66,18 +59,24 @@ def test_get_qc_criteria():
     nr_of_columns = list(criteria_df.columns)
     assert len(nr_of_columns) == 2, "number of columns != 2"
 
-def test_check_qc_criteria():
+def test_check_qc_criteria_and_output_csv():
     # GIVEN following variables
     config_file = "tests/test_data/qc_files/config_paired.json"
     hs_metrics = "tests/test_data/qc_files/multiqc_picard_HsMetrics.json"
+    output_path = "tests/test_data/qc_files/output.csv"
     qc_criteria_df = get_qc_criteria(read_qc_table(HSMETRICS_QC_CHECK), get_bait_name(config_file))
     hs_metrics_df = read_hs_metrics(hs_metrics)
     sample_names = get_sample_name(config_file)
 
-    # WHEN reading the function
+    # WHEN calling the function
     extract_criteria = check_qc_criteria(qc_criteria_df, hs_metrics_df,
                                          sample_names[0], sample_names[1])
 
-    # THEN check if the output df has 3 indexes
+    # THEN check if the output df has 3 indexes and if csv-file exists
     nr_of_indexes = list(extract_criteria.index)
     assert len(nr_of_indexes) == 3, "number of indexes != 3"
+
+    write_output(extract_criteria, output_path)
+    assert os.path.isfile(output_path), "File doesn't exists"
+
+
