5f15ff176ecb36feb6e4c78e33f647a151464ee6
ashwini06 <ashwini.jeggari@scilifelab.se>
2020-11-03 22:35:56 +0100

    add tests for density plot functions

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -27,6 +27,7 @@ from BALSAMIC.utils.rule import (
     get_variant_callers, get_script_path, get_result_dir, get_threads,
     get_delivery_id, get_reference_output_files)
 
+from BALSAMIC.utils.workflowscripts import get_file_contents, get_densityplot
 
 def test_get_variant_callers_wrong_analysis_type(tumor_normal_config):
     # GIVEN a wrong analysis_type
@@ -721,3 +722,41 @@ def test_get_fastq_bind_path(tmpdir_factory):
     create_fastq_symlink(casefiles=casefiles, symlink_dir=symlink_to_path)
     #THEN function returns list containing the original parent path!
     assert get_fastq_bind_path(symlink_to_path) == [symlink_from_path]
+
+
+def test_get_file_contents():
+    #GIVEN a test input file
+    test_file = 'tests/test_data/densityplots/dummy_file1.txt'
+    
+    # WHEN invoking function
+    test_file_built = get_file_contents(test_file,'umi')
+    column_names = ['id', 'AF', 'method']
+
+    # THEN check column names and no. of column matches
+    assert all(test_file_built.columns == column_names)
+    assert len(test_file_built.columns) == 3
+
+def test_get_wrongfile_contents():
+    #GIVEN a test input file
+    test_wrongfile = 'tests/test_data/densityplots/dummy_wrongfile.txt'
+
+    # WHEN invoking function
+    with pytest.raises(ValueError):
+        test_wrongfile_built = get_file_contents(test_wrongfile,'umi')
+        assert len(test_wrongfile_built.columns)!=3
+
+def test_get_densityplot():
+    #GIVEN prefix names, input and files
+    test_file1= 'tests/test_data/densityplots/dummy_file1.txt'
+    test_file2= 'tests/test_data/densityplots/dummy_file2.txt'
+    name1 = 'testnam1'
+    name2 = 'testnam2'
+    out_file='tests/test_data/densityplots/dummy_plot.pdf'
+    
+    # WHEN invoking function out_file is created
+    test_result = get_densityplot(test_file1, test_file2, name1, name2, out_file)
+
+    # THEN check for filepaths
+    assert Path(test_file1).exists()
+    assert Path(test_file2).exists()
+    assert Path(out_file).exists()
