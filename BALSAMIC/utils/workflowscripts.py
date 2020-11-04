8591d6cba6da67e603c364971a7b99981e8e9c43
ashwini06 <ashwini.jeggari@scilifelab.se>
2020-11-04 11:42:28 +0100

    fix correct variable names

--- BALSAMIC/utils/workflowscripts.py
+++ BALSAMIC/utils/workflowscripts.py
@@ -10,22 +10,22 @@ def get_file_contents(input_file, prefix_name):
     input_file: Path to the TSV file 
     """
 
-    file = pd.read_csv(input_file, sep='\t', header=None)
-    file.columns = ['id', 'AF']
-    file['method'] = prefix_name
-    return file
+    input_df = pd.read_csv(input_file, sep='\t', header=None)
+    input_df.columns = ['id', 'AF']
+    input_df['method'] = prefix_name
+    return input_df
 
 
 def get_densityplot(input_file1, input_file2, prefix_name1, prefix_name2,
-                    output_pdf):
+                    output_file):
     """ Reads two input dataframes and outputs a densityplot to the pdf file.
  
     Arguments:
     input_file1: Path to the TSV file1 (e.g: AFs calculated without consensuscall step) 
     input_file2: Path to the TSV file2 (e.g: AFs calculated after consensuscall step)
-    prefix_name1: Label legend w.r.t file1 (e.g: umiextract) 
-    prefix_name2: Label legend w.r.t file2 (e.g: consensuscall)
-    output_pdf: Path to output filename with .pdf extn
+    prefix_name1: Label legend w.r.t file1 (e.g: "umiextract") 
+    prefix_name2: Label legend w.r.t file2 (e.g: "consensuscall")
+    output_file: Path to output filename with '.pdf' extn (e.g: "outplot.pdf")
     """
 
     dataframe1 = get_file_contents(input_file1, prefix_name1)
@@ -40,4 +40,5 @@ def get_densityplot(input_file1, input_file2, prefix_name1, prefix_name2,
                 label=set(dataframe2['method']))
     plt.xlabel('Allelic Frequency (AF)')
     plt.ylabel('Probability Density')
-    return plt.savefig(output_pdf)
+    plt.savefig(output_file)
+    return output_file
