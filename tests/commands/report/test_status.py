81ce3b52faa45aaa7549940a4605ec30cca881c1
hassanfa <hassan.foroughi@gmail.com>
2020-11-12 22:25:41 +0100

    add tmp files for normal/tumor bam to check missing

--- tests/commands/report/test_status.py
+++ tests/commands/report/test_status.py
@@ -4,15 +4,17 @@ from unittest import mock
 
 def test_status_tumor_only_panel(invoke_cli, tumor_only_config,
                                  sentieon_install_dir, sentieon_license):
-    # GIVEN a tumor-normal config file
+    # GIVEN a tumor-only config file
     # WHEN running analysis
     with mock.patch.dict(
             'os.environ', {
                 'SENTIEON_LICENSE': sentieon_license,
                 'SENTIEON_INSTALL_DIR': sentieon_install_dir
             }):
-        result = invoke_cli(
-            ['report', 'status', '--sample-config', tumor_only_config])
+        result = invoke_cli([
+            'report', 'status', '--show-only-missing', '--sample-config',
+            tumor_only_config
+        ])
 
         # THEN it should run without any error
         assert result.exit_code == 0
