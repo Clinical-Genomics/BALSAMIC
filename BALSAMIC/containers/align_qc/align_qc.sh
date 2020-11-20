36d8b2ccb5549b5e740f9877d2568ec5362d0f26
hassanfa <hassan.foroughi@gmail.com>
2020-11-20 12:14:23 +0100

    proper conda env path

--- BALSAMIC/containers/align_qc/align_qc.sh
+++ BALSAMIC/containers/align_qc/align_qc.sh
@@ -1,9 +1,5 @@
-conda env create -n ${1} --file ${1}.yaml && \
-    source activate align_qc && \
-    picard_jar=picard-2.23.2-201-g922891d-SNAPSHOT-all.jar && \
-    picard_PATH=BALSAMIC/assets/${picard_jar} && \
-    picard_destination=/usr/local/miniconda/envs/align_qc/share/ && \
-    cp $picard_PATH ${picard_destination} && \
-    ln -s ${picard_destination}/${picard_jar} ${picard_destination}/picard.jar; \
-    ln -s /usr/local/miniconda/envs/align_qc/lib/libreadline.so.7.0 /usr/local/miniconda/envs/align_qc/lib/libreadline.so.6 && \
-    ln -s /usr/local/miniconda/envs/align_qc/lib/libreadline.so.7.0 /usr/local/miniconda/envs/align_qc/lib/libreadline.so.6.0
+conda env create -n ${1} --file ${1}.yaml
+source activate ${1}
+ENV_PATH=/opt/conda/envs
+ln -s ${ENV_PATH}/${1}/lib/libreadline.so.7.0 ${ENV_PATH}/${1}/lib/libreadline.so.6
+ln -s ${ENV_PATH}/${1}/lib/libreadline.so.7.0 ${ENV_PATH}/${1}/lib/libreadline.so.6.0
