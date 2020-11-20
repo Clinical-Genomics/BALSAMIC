eaed837238e578d5e006f8ae7f21d508e524d75e
hassanfa <hassan.foroughi@gmail.com>
2020-11-20 11:14:57 +0100

    proper env path

--- BALSAMIC/containers/varcall_py36/varcall_py36.sh
+++ BALSAMIC/containers/varcall_py36/varcall_py36.sh
@@ -1,4 +1,5 @@
 conda env create -n ${1} --file ${1}.yaml
 source activate ${1}
-ln -s /usr/local/miniconda/envs/${1}/lib/libreadline.so.7.0 /usr/local/miniconda/envs/${1}/lib/libreadline.so.6
-ln -s /usr/local/miniconda/envs/${1}/lib/libreadline.so.7.0 /usr/local/miniconda/envs/${1}/lib/libreadline.so.6.0
+ENV_PATH=/opt/conda/envs
+ln -s ${ENV_PATH}/${1}/lib/libreadline.so.7.0 ${ENV_PATH}/${1}/lib/libreadline.so.6
+ln -s ${ENV_PATH}/${1}/lib/libreadline.so.7.0 ${ENV_PATH}/${1}/lib/libreadline.so.6.0
