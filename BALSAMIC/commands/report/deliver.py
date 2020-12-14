d0ae9be22b68d5c7ce616f750532dbd3bc5b74b0
Mropat <mariaropat@gmail.com>
2020-12-14 16:36:33 +0100

    replace more tags

--- BALSAMIC/commands/report/deliver.py
+++ BALSAMIC/commands/report/deliver.py
@@ -154,12 +154,14 @@ def deliver(context, sample_config, analysis_type, rules_to_deliver, delivery_mo
 
     for file in delivery_json["files"]:
         for sample in sample_config_dict["samples"]:
-            if sample_config_dict["samples"][sample]["file_prefix"] in file["id"]:
-                file["id"] = sample["sample_name"]
+            file_prefix = sample_config_dict["samples"][sample]["file_prefix"]
+            sample_name = sample_config_dict["samples"][sample]["sample_name"]
+            if file_prefix == file["id"]:
+                file["id"] = sample_name
                 file["tag"] = (
                     file["tag"]
-                    .replace(sample["file_prefix"], sample["sample_name"])
-                    .replace(sample["file_prefix"].replace("-", "_"), sample["sample_name"])
+                    .replace(file_prefix, sample_name)
+                    .replace(file_prefix.replace("-", "_"), sample_name)
                 )
 
     # Add Housekeeper file to report
