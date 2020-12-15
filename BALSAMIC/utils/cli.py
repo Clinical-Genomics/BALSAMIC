422fca9c68dae205eeacaed6f76ddc9b4992ef5e
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2020-12-15 12:54:47 +0100

    Fix/tags as list (#527)
    * add sample names and propagate to devliverables
    
    * add github action tests
    
    * remove travis.yml
    
    * remove coveragerc
    
    * yaml hygeine
    
    * sentieon env var
    
    * coveragerc file
    
    * change sample name to sample_id
    
    * formatter
    
    * change sample_id to sample_name
    
    * add sample name to fixture
    
    * add options to fixture
    
    * fix index error
    
    * replace more tags
    
    * replace more tags
    
    * replace more tags
    
    * yapf the files
    
    * Update BALSAMIC/commands/config/case.py
    
    * make tags a mist
    
    * tests
    
    * tests
    
    * tags are now a list
    
    * yapf
    
    * all tags are a list
    
    * check if tags are a string
    
    * yapf
    
    Co-authored-by: hassanfa <hassan.foroughi@gmail.com>
    Co-authored-by: Hassan Foroughi <hassanfa@users.noreply.github.com>

--- BALSAMIC/utils/cli.py
+++ BALSAMIC/utils/cli.py
@@ -575,12 +575,15 @@ def convert_deliverables_tags(delivery_json: dict,
         for sample in sample_config_dict["samples"]:
             file_prefix = sample_config_dict["samples"][sample]["file_prefix"]
             sample_name = sample_config_dict["samples"][sample]["sample_name"]
+            if isinstance(file["tag"], str):
+                file_tags = file["tag"].split(",")
+            else:
+                file_tags = file["tag"]
             if file_prefix == file["id"]:
                 file["id"] = sample_name
-                file_tags = file["tag"].split(",")
                 for tag_index, tag in enumerate(file_tags):
                     if tag == file_prefix or tag == file_prefix.replace(
                             "_", "-"):
                         file_tags[tag_index] = sample_name
-                file["tag"] = file_tags
+            file["tag"] = file_tags
     return delivery_json
