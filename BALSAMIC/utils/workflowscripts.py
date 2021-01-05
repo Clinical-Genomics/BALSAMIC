b310701dd440d904d45abb7e227c9f315e93fa0c
hassanfa <hassan.foroughi@gmail.com>
2021-01-05 13:59:49 +0100

    beautify

--- BALSAMIC/utils/workflowscripts.py
+++ BALSAMIC/utils/workflowscripts.py
@@ -103,43 +103,44 @@ def plot_analysis(log_file: Path, h5_file: Path,
     df.plot(y="CPUUtilization",
             x="ElapsedTime",
             ax=cpu_ax,
-            style=cpu_ax_color + '--')
-    cpu_ax.set_label("Core usage")
+            color=cpu_ax_color,
+            style='--')
+    cpu_ax.set_title("CPU statistics")
     cpu_ax.set_xlabel("Wall seconds")
     cpu_ax.set_ylabel("Core usage (max {}%)".format(requested_cores * 100))
     cpu_ax.yaxis.label.set_color(cpu_ax_color)
     cpu_ax.yaxis.label.set_color(cpu_ax_color)
     cpu_ax.tick_params(axis='y', colors=cpu_ax_color)
+    cpu_ax.legend(loc="best", frameon=False)
+    cpu_ax.spines['top'].set_visible(False)
+    cpu_ax.spines['right'].set_visible(False)
     max_cpu_line = cpu_ax.axhline(requested_cores * 100,
                                   color=cpu_ax_color,
                                   ls='-')
     max_cpu_line.set_label("Max available")
-    cpu_ax.legend(loc="best", frameon=False)
-    cpu_ax.set_title("CPU statistics")
-    cpu_ax.spines['top'].set_visible(False)
-    cpu_ax.spines['right'].set_visible(False)
 
     mem_ax_color = 'g'
-    df.plot(y="VMSize", x="ElapsedTime", ax=mem_ax, style=mem_ax_color + "--")
+    df.plot(y="VMSize", x="ElapsedTime", ax=mem_ax, color=mem_ax_color, style="--")
+    mem_ax.set_title("Memory statistics")
     mem_ax.set_xlabel("Wall seconds")
     mem_ax.set_ylabel("Memory usage GB (max {}GB)".format(
         round(mem_per_core * requested_cores / 1024)))
     mem_ax.yaxis.label.set_color(mem_ax_color)
     mem_ax.yaxis.label.set_color(mem_ax_color)
     mem_ax.tick_params(axis='y', colors=mem_ax_color)
+    mem_ax.legend(loc="best", frameon=False)
+    mem_ax.spines['top'].set_visible(False)
+    mem_ax.spines['right'].set_visible(False)
     max_cpu_line = mem_ax.axhline(round(mem_per_core * requested_cores / 1024),
                                   color=mem_ax_color,
                                   ls='-')
     max_cpu_line.set_label("Max available mem")
-    mem_ax.legend(loc="best", frameon=False)
-    mem_ax.set_title("Memory statistics")
-    mem_ax.spines['top'].set_visible(False)
-    mem_ax.spines['right'].set_visible(False)
 
     read_io_ax_color = 'm'
     read_io_ax = df.plot(y="ReadMB",
                          x="ElapsedTime",
-                         style=read_io_ax_color + "--",
+                         color=read_io_ax_color,
+                         style="--",
                          ax=io_ax,
                          legend=False)
     read_io_ax.set_xlabel("Wall seconds")
@@ -150,18 +151,19 @@ def plot_analysis(log_file: Path, h5_file: Path,
     read_io_ax.spines['top'].set_visible(False)
 
     write_io_ax = read_io_ax.twinx()
-    write_io_ax_color = 'y'
+    write_io_ax_color = 'olive'
     write_io_ax = df.plot(y="WriteMB",
                           x="ElapsedTime",
                           ax=write_io_ax,
-                          style=write_io_ax_color + "--")
+                          color=write_io_ax_color,
+                          style="--", legend=False)
+    write_io_ax.set_title("Disk I/O statistics")
     write_io_ax.set_xlabel("Wall seconds")
     write_io_ax.set_ylabel("Disk write (MB)")
     write_io_ax.yaxis.label.set_color(write_io_ax_color)
     write_io_ax.yaxis.label.set_color(write_io_ax_color)
     write_io_ax.tick_params(axis='y', colors=write_io_ax_color)
     write_io_ax.yaxis.tick_right()
-    write_io_ax.set_title("Disk I/O statistics")
     write_io_ax.spines['top'].set_visible(False)
 
     handles, labels = [], []
