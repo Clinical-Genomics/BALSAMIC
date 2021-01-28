49a8a9f1a02d10adb0c3bff16993dd9134227419
hassanfa <hassan.foroughi@gmail.com>
2021-01-28 10:30:11 +0100

    codefactor comments

--- BALSAMIC/utils/qc_report.py
+++ BALSAMIC/utils/qc_report.py
@@ -45,7 +45,7 @@ def render_html(meta: dict, html_out: str):
 
     markdown_text = md_template.convert(source=report_body)
 
-    env = Environment(loader=FileSystemLoader(template_path))
+    env = Environment(loader=FileSystemLoader(template_path), autoescape=True)
 
     template = env.get_template("balsamic_report.html")
 
@@ -57,7 +57,7 @@ def render_html(meta: dict, html_out: str):
         
 def render_body(meta: dict, template_path: str, body_template_md: str = "balsamic_report.md") -> str:
     """renders text body of the report from a markdown template"""
-    env = Environment(loader=FileSystemLoader(template_path))
+    env = Environment(loader=FileSystemLoader(template_path), autoescape=True)
 
     template = env.get_template(body_template_md)
 
