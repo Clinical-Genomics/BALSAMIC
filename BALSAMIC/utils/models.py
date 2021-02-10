0bf4ba9ad46b806d66fb112fcb53110249d51dd7
ashwini06 <ashwini.jeggari@scilifelab.se>
2021-02-10 12:13:43 +0100

    update attributes of VarCallerFilter

--- BALSAMIC/utils/models.py
+++ BALSAMIC/utils/models.py
@@ -45,7 +45,10 @@ class VarCallerFilter(BaseModel):
         MQ: VCFAttributes (optional); minimum mapping quality
         DP: VCFAttributes (optional); minimum read depth
         pop_freq: VCFAttributes (optional); maximum gnomad_af
-        varcaller_name: str (required); variant caller name
+	cnt_reads: VCFAttributes (optional); minimum read counts 
+        qss: VCFAttributes (optional); minimum sum of base quality scores
+	sor: VCFAttributes (optional); minimum symmetrical log-odds ratio  
+	varcaller_name: str (required); variant caller name
         filter_type: str (required); filter name for variant caller
         analysis_type: str (required); analysis type e.g. tumor_normal or tumor_only
         description: str (required); comment section for description
@@ -57,6 +60,9 @@ class VarCallerFilter(BaseModel):
     MQ: Optional[VCFAttributes]
     DP: Optional[VCFAttributes]
     pop_freq: VCFAttributes
+    cnt_reads: Optional[VCFAttributes]
+    qss : Optional[VCFAttributes]
+    sor : Optional[VCFAttributes]
     varcaller_name: str
     filter_type: str
     analysis_type: str
