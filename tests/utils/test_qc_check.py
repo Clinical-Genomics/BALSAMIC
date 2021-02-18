bee89d7da03f04141747978ea548a4afcc5f3b7a
Keyvan Elhami <keyvan.elhami@scilifelab.se>
2021-02-18 15:41:42 +0100

    Added new test func and import modules

--- tests/utils/test_qc_check.py
+++ tests/utils/test_qc_check.py
@@ -5,10 +5,11 @@ from pathlib import Path
 from BALSAMIC.utils.cli import write_json
 from BALSAMIC.utils.qc_check import read_json, read_qc_table, check_qc_criteria, write_output
 from BALSAMIC.utils.qc_check import get_bait_name, get_sample_name, get_qc_criteria, failed_qc
+from BALSAMIC.utils.qc_check import get_qc_value, check_qc_criteria_simple
 from BALSAMIC.utils.constants import HSMETRICS_QC_CHECK
 
 
-def test_read_hs_metrics():
+def test_read_json():
     # GIVEN the file exist
     hs_metrics_path = "tests/test_data/qc_files/multiqc_picard_HsMetrics.json"
 
@@ -32,6 +33,20 @@ def test_read_qc_table():
         assert bol_list[n], "No values exists"
 
 
+def test_get_qc_value_and_qc_criteria_simple():
+    # GIVEN the file exist
+    hs_metrics_path = "tests/test_data/qc_files/multiqc_picard_HsMetrics.json"
+    fold80 = "tests/test_data/qc_files/fold_80.txt"
+
+    # WHEN reading the file
+    qc_list = get_qc_value(read_json(hs_metrics_path))
+    qc_criteria = check_qc_criteria_simple(qc_list, fold80)
+
+    #THEN check if the qc-list in not empty and the qc-criteria is bool
+    assert len(qc_list) > 0, "qc_list is empty"
+    assert type(qc_criteria) == bool, "qc_criteria is not Boolean"
+
+
 def test_get_bait_and_sample_name(tumor_normal_config):
     # GIVEN the file exists
     # WHEN reading the file
@@ -43,7 +58,7 @@ def test_get_bait_and_sample_name(tumor_normal_config):
     assert sample_name[0], "sample name doesn't exist"
 
 
-def test_get_qc_criteria():
+def test_check_qc_criteria():
     # GIVEN following df and bed
     df_qc = read_qc_table(HSMETRICS_QC_CHECK)
     bed = "gmcksolid_4.1_hg19_design.bed"
