db390690f111851d6caa7ca028986238d2f8c707
ashwini06 <ashwini.jeggari@scilifelab.se>
2021-04-12 15:08:47 +0200

    add pon as analysis-type

--- BALSAMIC/utils/constants.py
+++ BALSAMIC/utils/constants.py
@@ -34,7 +34,7 @@ SENTIEON_TNSCOPE = Path(
 # Analysis related constants
 MUTATION_CLASS = ["somatic", "germline"]
 MUTATION_TYPE = ["SNV", "SV", "CNV"]
-ANALYSIS_TYPES = ["paired", "single", "umi", "qc"]
+ANALYSIS_TYPES = ["paired", "single", "umi", "qc", "pon"]
 WORKFLOW_SOLUTION = ["BALSAMIC", "Sentieon", "DRAGEN", "Sentieon_umi"]
 
 # Variantcaller parameters
