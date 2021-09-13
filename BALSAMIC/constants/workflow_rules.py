bca56bb7b94c5194a2bf344ca8ccbb0d31227f73
hassanfa <hassan.foroughi@gmail.com>
2021-09-13 17:45:09 +0200

    black linting

--- BALSAMIC/constants/workflow_rules.py
+++ BALSAMIC/constants/workflow_rules.py
@@ -1,19 +1,18 @@
 # Define set of rules
 SNAKEMAKE_RULES = {
     "common": {
-        "qc":
-            [
+        "qc": [
             "snakemake_rules/quality_control/fastp.rule",
             "snakemake_rules/quality_control/fastqc.rule",
             "snakemake_rules/quality_control/multiqc.rule",
             "snakemake_rules/variant_calling/mergetype_tumor.rule",
-            ],
-        "align":[],
-        "varcall":["snakemake_rules/variant_calling/germline_sv.rule"],
-        "annotate":["snakemake_rules/annotation/vep.rule"],
+        ],
+        "align": [],
+        "varcall": ["snakemake_rules/variant_calling/germline_sv.rule"],
+        "annotate": ["snakemake_rules/annotation/vep.rule"],
     },
-    "single_targeted":{
-        "qc":[
+    "single_targeted": {
+        "qc": [
             "snakemake_rules/quality_control/GATK.rule",
             "snakemake_rules/quality_control/picard.rule",
             "snakemake_rules/quality_control/sambamba_depth.rule",
@@ -22,12 +21,12 @@ SNAKEMAKE_RULES = {
             "snakemake_rules/umi/mergetype_tumor_umi.rule",
             "snakemake_rules/umi/generate_AF_tables.rule",
         ],
-        "align":[
+        "align": [
             "snakemake_rules/align/bwa_mem.rule",
             "snakemake_rules/umi/sentieon_umiextract.rule",
             "snakemake_rules/umi/sentieon_consensuscall.rule",
         ],
-        "varcall":[
+        "varcall": [
             "snakemake_rules/variant_calling/germline.rule",
             "snakemake_rules/variant_calling/split_bed.rule",
             "snakemake_rules/variant_calling/cnvkit_single.rule",
@@ -35,14 +34,14 @@ SNAKEMAKE_RULES = {
             "snakemake_rules/variant_calling/somatic_sv_tumor_only.rule",
             "snakemake_rules/umi/sentieon_varcall_tnscope.rule",
         ],
-        "annotate":[
+        "annotate": [
             "snakemake_rules/annotation/rankscore.rule",
             "snakemake_rules/annotation/varcaller_sv_filter.rule",
             "snakemake_rules/annotation/varcaller_filter_tumor_only.rule",
         ],
     },
-    "paired_targeted":{
-        "qc":[
+    "paired_targeted": {
+        "qc": [
             "snakemake_rules/quality_control/GATK.rule",
             "snakemake_rules/quality_control/picard.rule",
             "snakemake_rules/quality_control/sambamba_depth.rule",
@@ -54,12 +53,12 @@ SNAKEMAKE_RULES = {
             "snakemake_rules/quality_control/contest.rule",
             "snakemake_rules/umi/generate_AF_tables.rule",
         ],
-        "align":[
+        "align": [
             "snakemake_rules/align/bwa_mem.rule",
             "snakemake_rules/umi/sentieon_umiextract.rule",
             "snakemake_rules/umi/sentieon_consensuscall.rule",
         ],
-        "varcall":[
+        "varcall": [
             "snakemake_rules/variant_calling/germline.rule",
             "snakemake_rules/variant_calling/split_bed.rule",
             "snakemake_rules/variant_calling/somatic_tumor_normal.rule",
@@ -67,44 +66,44 @@ SNAKEMAKE_RULES = {
             "snakemake_rules/variant_calling/cnvkit_paired.rule",
             "snakemake_rules/umi/sentieon_varcall_tnscope_tn.rule",
         ],
-        "annotate":[
+        "annotate": [
             "snakemake_rules/annotation/rankscore.rule",
             "snakemake_rules/annotation/varcaller_sv_filter.rule",
             "snakemake_rules/annotation/varcaller_filter_tumor_normal.rule",
         ],
     },
-    "single_wgs":{
-        "qc":[
+    "single_wgs": {
+        "qc": [
             "snakemake_rules/quality_control/sentieon_qc_metrics.rule",
-            "snakemake_rules/quality_control/picard_wgs.rule"
+            "snakemake_rules/quality_control/picard_wgs.rule",
         ],
-        "align":["snakemake_rules/align/sentieon_alignment.rule"],
-        "varcall":[
+        "align": ["snakemake_rules/align/sentieon_alignment.rule"],
+        "varcall": [
             "snakemake_rules/variant_calling/sentieon_germline.rule",
             "snakemake_rules/variant_calling/sentieon_split_snv_sv.rule",
             "snakemake_rules/variant_calling/sentieon_t_varcall.rule",
             "snakemake_rules/variant_calling/somatic_sv_tumor_only.rule",
             "snakemake_rules/dragen_suite/dragen_dna.rule",
         ],
-        "annotate":[
+        "annotate": [
             "snakemake_rules/annotation/varcaller_wgs_filter_tumor_only.rule",
             "snakemake_rules/annotation/varcaller_sv_wgs_filter_tumor_only.rule",
         ],
     },
-    "paired_wgs":{
-        "qc":[
+    "paired_wgs": {
+        "qc": [
             "snakemake_rules/quality_control/sentieon_qc_metrics.rule",
             "snakemake_rules/quality_control/picard_wgs.rule",
             "snakemake_rules/variant_calling/mergetype_normal.rule",
         ],
-        "align":["snakemake_rules/align/sentieon_alignment.rule"],
-        "varcall":[
+        "align": ["snakemake_rules/align/sentieon_alignment.rule"],
+        "varcall": [
             "snakemake_rules/variant_calling/sentieon_germline.rule",
             "snakemake_rules/variant_calling/sentieon_split_snv_sv.rule",
             "snakemake_rules/variant_calling/sentieon_tn_varcall.rule",
             "snakemake_rules/variant_calling/somatic_sv_tumor_normal.rule",
         ],
-        "annotate":[
+        "annotate": [
             "snakemake_rules/annotation/varcaller_wgs_filter_tumor_normal.rule",
             "snakemake_rules/annotation/varcaller_sv_wgs_filter_tumor_normal.rule",
         ],
