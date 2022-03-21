2e7b0edcb04e51807e45f0470156a22d650e1ada
ashwini06 <ashwini.jeggari@scilifelab.se>
2022-03-21 11:34:42 +0100

    fix black

--- tests/conftest.py
+++ tests/conftest.py
@@ -101,6 +101,7 @@ def background_variant_file():
 def pon_cnn_file():
     return "tests/test_data/references/panel/test_panel_ponn.cnn"
 
+
 @pytest.fixture(scope="session")
 def sentieon_license(tmp_path_factory):
     """
@@ -349,7 +350,7 @@ def tumor_only_config(
     panel_bed_file,
     sentieon_license,
     sentieon_install_dir,
-    pon_cnn_file
+    pon_cnn_file,
 ):
     """
     invokes balsamic config sample -t xxx to create sample config
@@ -384,7 +385,7 @@ def tumor_only_config(
                 "--background-variants",
                 background_variant_file,
                 "--pon-cnn",
-                pon_cnn_file
+                pon_cnn_file,
             ],
         )
 
