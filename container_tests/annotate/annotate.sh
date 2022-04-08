6391e14d8cb385f79802e31200b7678e5326e8e3
ashwini06 <ashwini.jeggari@scilifelab.se>
2022-04-08 16:22:58 +0200

    fix spaces and ticks

--- container_tests/annotate/annotate.sh
+++ container_tests/annotate/annotate.sh
@@ -5,12 +5,12 @@ valid_commands=( "bcftools" "vcfanno" "vcf2cytosure" "genmod" "vep" "vep_install
 
 for tool in ${valid_commands[@]}
 do
-	tool_msg=`$tool --help 2>&1 | grep 'No'`
-	if [[ -z $tool_msg ]]; then 
-		echo "$tool command exists and works";
-	else 
-		echo "$tool command doesnt work";
-		exit 1;
-	fi
+    tool_msg=$($tool --help 2>&1 | grep 'No')
+    if [[ -z $tool_msg ]]; then 
+        echo "$tool command exists and works";
+    else 
+        echo "$tool command doesnt work";
+        exit 1;
+    fi
 done
 
