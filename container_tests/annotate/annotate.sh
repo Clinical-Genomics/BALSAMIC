72a32d4d41661c29829bd911995cd2389bbd6575
ashwini06 <ashwini.jeggari@scilifelab.se>
2022-04-11 12:35:28 +0200

    update container tests

--- container_tests/annotate/annotate.sh
+++ container_tests/annotate/annotate.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Test if commands exist
 
-valid_commands=( "bcftools" "vcfanno" "genmod" "vep" "vep_install" )
+valid_commands=( "bcftools" "vcfanno" "genmod" "vep" "vep_install")
 
-for tool in ${valid_commands[@]}
+for tool in "${valid_commands[@]}"
 do
-    tool_msg=$($tool --help 2>&1 | grep 'No')
-    if [[ -z $tool_msg ]]; then 
-        echo "$tool command exists and works";
-    else 
-        echo "$tool command doesnt work";
+    if ! command -v "${tool}"  &> /dev/null; then
+        echo "$tool command not found in the container"
         exit 1;
+    else
+        tool_msg=`$tool 2>&1 | grep 'No'`
+        if [[ -z $tool_msg ]];then
+            echo "$tool command found and \"$tool --help\" command executes"
+        else
+            echo "$tool found in the container but \"$tool --help\" command not executing"
+            exit 1;
+        fi
     fi
 done
-
