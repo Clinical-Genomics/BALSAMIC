66e20eb53ea4ed3646cfcae90b05e2fa9d611c4f
ashwini06 <ashwini.jeggari@scilifelab.se>
2022-05-20 20:27:30 +0200

    black formatted

--- BALSAMIC/commands/config/case.py
+++ BALSAMIC/commands/config/case.py
@@ -134,7 +134,7 @@ LOG = logging.getLogger(__name__)
     help=(
         "Genome version to prepare reference. Path to genome"
         "will be <outdir>/genome_version"
-    )
+    ),
 )
 @click.option(
     "-w",
@@ -147,7 +147,7 @@ LOG = logging.getLogger(__name__)
         "workflow will be running. If you want to run both "
         "balsamic and UMI workflow together for panel data; "
         'choose "balsamic-umi" option '
-    )
+    ),
 )
 @click.pass_context
 def case_config(
@@ -204,7 +204,7 @@ def case_config(
             "analysis_dir": analysis_dir,
             "analysis_type": "paired" if normal else "single",
             "sequencing_type": "targeted" if panel_bed else "wgs",
-            "analysis_workflow": analysis_workflow
+            "analysis_workflow": analysis_workflow,
         },
         reference=reference_dict,
         singularity=os.path.join(balsamic_cache, balsamic_version, "containers"),
