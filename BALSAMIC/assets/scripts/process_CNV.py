376b9d7a3faa6825bba57d8dab5a76bdb267e3b9
khurrammaqbool <khurram.maqbool@outlook.com>
2022-06-07 15:55:32 +0200

    lint using black

--- BALSAMIC/assets/scripts/process_CNV.py
+++ BALSAMIC/assets/scripts/process_CNV.py
@@ -112,7 +112,7 @@ def get_calls(sv):
                 CALL[key] = ",".join(map(str, value))
             else:
                 CALL[key] = value
-        calls += str(':'.join(map(str, CALL.values())))
+        calls += str(":".join(map(str, CALL.values())))
         calls += "\t"
     calls = calls.rstrip()
     return calls
