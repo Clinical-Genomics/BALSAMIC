6389432e3449cf2b234605e2c54d93a01c11e3cb
khurrammaqbool <khurram.maqbool@outlook.com>
2022-06-07 16:49:51 +0200

    fix bare exception

--- BALSAMIC/assets/scripts/process_CNV.py
+++ BALSAMIC/assets/scripts/process_CNV.py
@@ -62,7 +62,7 @@ def main(vcf_file, cnv_caller, tumor_sample, normal_sample):
             for sv in vcf:
                 SV = get_sv(sv, cnv_caller, samples, tumor, normal)
                 write_sv(SV)
-        except:
+        except Exception:
             sys.exit("Please provide valid VCF file")
     else:
         print("Please provide ascat or delly VCF file for CNVs")
