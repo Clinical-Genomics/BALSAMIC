8742301c8ec104277d9f981d6846b276809a29fe
ashwini06 <ashwini.jeggari@scilifelab.se>
2022-07-13 15:41:02 +0200

    revert automock cyvcf2 imports to conf

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Modules to be mocked up
+autodoc_mock_imports = ["cyvcf2"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
