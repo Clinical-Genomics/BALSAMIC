4b2b9835d1eaa64e46195efdd12a7b94f6929ec7
Eva Caceres <eva.caceres@scilifelab.se>
2022-11-15 13:12:17 +0100

    Only collect relatedness metric if somalier is part of the multiqc

--- BALSAMIC/assets/scripts/collect_qc_metrics.py
+++ BALSAMIC/assets/scripts/collect_qc_metrics.py
@@ -50,8 +50,9 @@ def collect_qc_metrics(
         metrics += get_variant_metrics(count)
 
     # Relatedness
-    analysis_type = get_analysis_type(config)
-    if analysis_type == "paired":
+    # analysis_type = get_analysis_type(config)
+    # if analysis_type == "paired":
+    if multiqc_data["report_data_sources"]["Somalier"]:
         metrics += get_relatedness_metrics(multiqc_data)
 
     with open(output_path, "w") as fn:
