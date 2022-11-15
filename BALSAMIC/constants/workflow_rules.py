62a79f91deebfd7d4e9dc69eeb1b782964d43423
Eva Caceres <eva.caceres@scilifelab.se>
2022-11-15 16:50:36 +0100

    Fix mixup in workflow rules

--- BALSAMIC/constants/workflow_rules.py
+++ BALSAMIC/constants/workflow_rules.py
@@ -4,6 +4,7 @@ SNAKEMAKE_RULES = {
         "qc": [
             "snakemake_rules/quality_control/fastp.rule",
             "snakemake_rules/quality_control/fastqc.rule",
+            "snakemake_rules/quality_control/multiqc.rule",
             "snakemake_rules/quality_control/qc_metrics.rule",
             "snakemake_rules/variant_calling/mergetype_tumor.rule",
         ],
@@ -49,7 +50,6 @@ SNAKEMAKE_RULES = {
     },
     "paired_targeted": {
         "qc": [
-            "snakemake_rules/quality_control/multiqc.rule",
             "snakemake_rules/quality_control/GATK.rule",
             "snakemake_rules/quality_control/picard.rule",
             "snakemake_rules/quality_control/sambamba_depth.rule",
@@ -100,7 +100,6 @@ SNAKEMAKE_RULES = {
     },
     "paired_wgs": {
         "qc": [
-            "snakemake_rules/quality_control/multiqc.rule",
             "snakemake_rules/quality_control/sentieon_qc_metrics.rule",
             "snakemake_rules/quality_control/picard_wgs.rule",
             "snakemake_rules/variant_calling/mergetype_normal.rule",
