0c19b23847547aecb6b5785a7ec9c9579618fc52
Vadym Ivanchuk <vadym.ivanchuk@scilifelab.se>
2022-11-21 19:30:07 +0100

    reformat script

--- BALSAMIC/assets/scripts/generate_cnv_report.py
+++ BALSAMIC/assets/scripts/generate_cnv_report.py
@@ -1,43 +1,80 @@
 #!/usr/bin/env python
 import os
+from pathlib import Path
+from typing import List
+
 import click
 
 from fpdf import FPDF
 from PIL import Image
 
 
-@click.command(
-    short_help="Merge images and a txt file into a single PDF",
-)
-@click.argument("output", type=click.Path(exists=False), required=True)
-@click.argument("data", type=click.Path(exists=True), required=True)
-@click.argument("images", nargs=-1, type=click.Path(exists=True), required=True)
-def generate_cnv_report(output, data, images):
-    pdf = generate_fpdf()
-    pdf = add_table_pdf(pdf, data)
-    pdf = add_images_pdf(pdf, images)
+@click.command(short_help="Merge statistics and plots into a single CNV report")
+@click.argument("statistics", nargs=1, type=click.Path(exists=True), required=False)
+@click.argument("plots", nargs=-1, type=click.Path(exists=True), required=False)
+@click.option("-o", "--output", type=click.Path(exists=False), required=True)
+def generate_cnv_report(statistics: Path, plots: List[Path], output: Path) -> None:
+    """Generate a CNV report given a set of statistic files and a list of plots."""
+    pdf: PDF = get_pdf_instance()
+    pdf: PDF = add_data_to_pdf(pdf=pdf, data_path=statistics) if statistics else pdf
+    pdf: PDF = add_plots_to_pdf(pdf=pdf, plot_paths=plots) if plots else pdf
     pdf.output(output)
 
 
 class PDF(FPDF):
+    """PDF generation subclass."""
+
     def footer(self):
+        """Overwrite the predetermined method to perform a specific footer processing."""
         self.set_y(-15)
         self.set_font("helvetica", "I", 8)
         self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", 0, 0, "C")
 
 
-def generate_fpdf():
-    pdf = PDF()
+def get_pdf_instance() -> PDF:
+    """Return a PDF instance."""
+    pdf: PDF = PDF()
     pdf.alias_nb_pages(alias="{nb}")
     return pdf
 
 
-def add_images_pdf(pdf, img_paths):
+def add_data_to_pdf(pdf: PDF, data_path: Path) -> PDF:
+    """Add statistics to a PDF instance."""
+    with open(data_path) as data:
+        data = data.readlines()
+    pdf.add_page()
     pdf.set_font("helvetica", "B", 15)
+    # Title layout & styling
+    title = os.path.basename(data_path).replace(".txt", "")
+    pdf.cell(25)
+    pdf.cell(140, 10, title, 1, 0, "C")
+    pdf.cell(35, 25, ln=1)  # Post title indentation
+    # Table layout & styling
+    pdf.set_font("Times", size=11)
+    line_height = pdf.font_size * 2.5
+    col_width = pdf.epw / 4  # Even distribution of the content
+    for row in data:
+        pdf.cell(45)
+        for statistic in row.split():
+            pdf.multi_cell(
+                col_width,
+                line_height,
+                statistic,
+                align="C",
+                border=1,
+                ln=3,
+                max_line_height=pdf.font_size,
+            )
+        pdf.ln(line_height)
 
-    for path in img_paths:
+    return pdf
+
+
+def add_plots_to_pdf(pdf: PDF, plot_paths: List[Path]) -> PDF:
+    """Add plots to a PDF instance."""
+    pdf.set_font("helvetica", "B", 15)
+    for path in plot_paths:
         title = os.path.basename(path).replace(".png", "")
-
         # Image & page layout parameters
         if "sunrise" in title:
             page_orientation = "portrait"
@@ -62,41 +99,6 @@ def add_images_pdf(pdf, img_paths):
         img = Image.open(path)
         img.thumbnail(img_size, Image.ANTIALIAS)
         pdf.image(img, img_xy[0], img_xy[1])
-
-    return pdf
-
-
-def add_table_pdf(pdf, data_path):
-
-    with open(data_path) as data:
-        data = data.readlines()
-
-    pdf.add_page()
-    pdf.set_font("helvetica", "B", 15)
-
-    # Title layout & styling
-    title = os.path.basename(data_path).replace(".txt", "")
-    pdf.cell(25)
-    pdf.cell(140, 10, title, 1, 0, "C")
-    pdf.cell(35, 25, ln=1)  # Post title indentation
-
-    # Table layout & styling
-    pdf.set_font("Times", size=11)
-    line_height = pdf.font_size * 2.5
-    col_width = pdf.epw / 4  # Even distribution of the content
-    for row in data:
-        pdf.cell(45)
-        for statistic in row.split():
-            pdf.multi_cell(
-                col_width,
-                line_height,
-                statistic,
-                align="C",
-                border=1,
-                ln=3,
-                max_line_height=pdf.font_size,
-            )
-        pdf.ln(line_height)
 
     return pdf
 
--- tests/scripts/test_create_pdf.py
+++ tests/scripts/test_create_pdf.py
@@ -1,71 +1,84 @@
 from pathlib import Path
 
-from fpdf import FPDF
-
 from BALSAMIC.assets.scripts.generate_cnv_report import (
-    generate_fpdf,
-    add_images_pdf,
-    add_table_pdf,
+    get_pdf_instance,
+    add_data_to_pdf,
+    add_plots_to_pdf,
     generate_cnv_report,
+    PDF,
 )
 
 
-def test_generate_fpdf():
+def test_get_pdf_instance():
+    """Test FPDF instance generation."""
+
     # WHEN creating a dummy FPDF file
-    pdf = generate_fpdf()
+    pdf: PDF = get_pdf_instance()
 
-    # THEN check if the pdf has been correctly created
-    assert isinstance(pdf, FPDF)
-
-
-def test_add_images_pdf():
-    # GIVEN ascatNGgs output PNG images
-    test_images_path = [
-        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.sunrise.png",
-        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.germline.png",
-    ]
-
-    # WHEN calling the function
-    pdf = add_images_pdf(generate_fpdf(), test_images_path)
-
-    # THEN check if the images are appended to the PDF
-    assert isinstance(pdf, FPDF)
-    assert pdf.page_no() == 2
+    # THEN check if the PDF has been correctly created
+    assert isinstance(pdf, PDF)
 
 
-def test_add_table_pdf():
-    # GIVEN ascatNGgs output sample statistics .txt
-    test_statistics_path = (
-        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.samplestatistics.txt"
-    )
+def test_add_data_to_pdf():
+    """Test add statistics to a PDF instance."""
 
-    # WHEN calling the function
-    pdf = add_table_pdf(generate_fpdf(), test_statistics_path)
-
-    # THEN check if the table is appended to the created PDF
-    assert isinstance(pdf, FPDF)
-    assert pdf.page_no() == 1
-
-
-def test_generate_cnv_report(tmp_path, cli_runner):
-    # GIVEN ascatNGgs output statistics
+    # GIVEN a PDF instance and an output sample statistics .txt file
+    pdf: PDF = get_pdf_instance()
     statistics_path = (
         "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.samplestatistics.txt"
     )
 
-    # GIVEN ascatNGgs output plots
-    plots_path = [
+    # WHEN generating the PDF with the statistics
+    pdf: PDF = add_data_to_pdf(pdf=pdf, data_path=statistics_path)
+
+    # THEN check if the statistics are appended to the created PDF
+    assert isinstance(pdf, PDF)
+    assert pdf.page_no() == 1
+
+
+def test_add_plots_to_pdf():
+    """Test plots appending to a PDF file."""
+
+    # GIVEN a PDF instance and some dummy PNG plots
+    pdf: PDF = get_pdf_instance()
+    plot_paths = [
+        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.sunrise.png",
+        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.germline.png",
+    ]
+
+    # WHEN adding the plots to a PDF instance
+    pdf: PDF = add_plots_to_pdf(pdf, plot_paths)
+
+    # THEN check if the images are correctly appended to the PDF
+    assert isinstance(pdf, PDF)
+    assert pdf.page_no() == len(plot_paths)
+
+
+def test_generate_cnv_report(tmp_path, cli_runner):
+    """Test generation of a PDF report."""
+
+    # GIVEN dummy input data and plots
+    statistics_path = (
+        "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.samplestatistics.txt"
+    )
+    plot_paths = [
         "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.germline.png",
         "tests/test_data/ascat_output/CNV.somatic.SAMPLE.ascat.sunrise.png",
     ]
 
     # GIVEN the output path
-    output_path = tmp_path / "ascat.output.pdf"
+    output_path: Path = Path(tmp_path, "ascat.output.pdf")
 
     # WHEN invoking the python script
     result = cli_runner.invoke(
         generate_cnv_report,
-        [str(output_path), statistics_path, plots_path[0], plots_path[1]],
+        [
+            statistics_path,
+            plot_paths[0],
+            plot_paths[1],
+            "--output",
+            output_path,
+        ],
     )
 
     # THEN check if the PDF is correctly created and there is no errors
