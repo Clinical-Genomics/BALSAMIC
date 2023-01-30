c33fa74729efbae0472e204dc5057bb91d796d1b
Eva Caceres <eva.caceres@scilifelab.se>
2023-01-30 16:19:22 +0100

    Revert all containers

--- BALSAMIC/containers/annotate/annotate.sh
+++ BALSAMIC/containers/annotate/annotate.sh
@@ -1,1 +1,2 @@
 conda env update -n base --file ${1}.yaml --prune
+pip install --no-cache-dir genmod==3.7.4
--- BALSAMIC/containers/varcall_cnvkit/varcall_cnvkit.sh
+++ BALSAMIC/containers/varcall_cnvkit/varcall_cnvkit.sh
@@ -1,1 +1,2 @@
 conda env update -n base --file ${1}.yaml --prune
+pip install --no-cache-dir cnvkit==0.9.9 biopython==1.79
