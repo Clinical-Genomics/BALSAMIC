1f0cb91b7dbfa893e6bad1fe05088262ac912677
Vadym <vadym.ivanchuk@scilifelab.se>
2023-06-27 10:56:44 +0200

    remove identations

--- BALSAMIC/utils/metrics.py
+++ BALSAMIC/utils/metrics.py
@@ -1,9 +1,7 @@
 """QC metrics utility methods."""
-
 from BALSAMIC.models.metrics import MetricValidationModel
 
 
 def validate_qc_metrics(metrics: dict) -> dict:
-    """Returns a set of validated QC metrics"""
-
+    """Returns a set of validated QC metrics."""
     return MetricValidationModel(metrics=metrics).dict()["metrics"]
