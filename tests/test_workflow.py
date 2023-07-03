362662921b3b5d0a91ee40e82b412bb3f5223e3b
khurrammaqbool <khurram.maqbool@outlook.com>
2023-07-03 14:35:26 +0200

    refactor names and comments

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -6,18 +6,18 @@ from BALSAMIC.utils.cli import get_snakefile
 MOCKED_OS_ENVIRON = "os.environ"
 
 
-def test_workflow_tumor_only_tga(
+def test_workflow_tumor_only_tga_hg19(
     tumor_only_config, sentieon_install_dir, sentieon_license
 ):
-    # GIVEN a sample config dict and snakefile
+    # GIVEN a sample config dict and a snakefile
     analysis_type = "single"
     reference_genome = "hg19"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow, reference_genome)
     config_json = tumor_only_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for TGA, hg19-tumor-only should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
         {
@@ -28,18 +28,18 @@ def test_workflow_tumor_only_tga(
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_tumor_normal_tga(
+def test_workflow_tumor_normal_tga_hg19(
     tumor_normal_config, sentieon_install_dir, sentieon_license
 ):
-    # GIVEN a sample config dict and snakefile
+    # GIVEN a sample config dict and a snakefile
     analysis_type = "paired"
     reference_genome = "hg19"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow, reference_genome)
     config_json = tumor_normal_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for TGA, hg19-tumor-normal should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
         {
@@ -50,18 +50,18 @@ def test_workflow_tumor_normal_tga(
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_tumor_only_wgs(
+def test_workflow_tumor_only_wgs_hg19(
     tumor_only_wgs_config, sentieon_install_dir, sentieon_license
 ):
-    # GIVEN a sample config dict and snakefile
+    # GIVEN a sample config dict and a snakefile
     analysis_type = "single"
     reference_genome = "hg19"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow, reference_genome)
     config_json = tumor_only_wgs_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for WGS, hg19-tumor-only should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
         {
@@ -72,18 +72,18 @@ def test_workflow_tumor_only_wgs(
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_tumor_normal_wgs(
+def test_workflow_tumor_normal_wgs_hg19(
     tumor_normal_wgs_config, sentieon_install_dir, sentieon_license
 ):
-    # GIVEN a sample config dict and snakefile
+    # GIVEN a sample config dict and a snakefile
     analysis_type = "paired"
     reference_genome = "hg19"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow, reference_genome)
     config_json = tumor_normal_wgs_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for WGS, hg19-tumor-normal should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
         {
@@ -94,49 +94,65 @@ def test_workflow_tumor_normal_wgs(
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_qc_tumor_only_canfam(tumor_only_config):
-
-    # GIVEN a sample config dict and snakefile
-    workflow = "single"
-    reference_genome = "canfam3"
+def test_workflow_qc_tumor_only_hg19(tumor_only_config):
+    # GIVEN a sample config dict and a snakefile
+    workflow = "paired"
+    reference_genome = "hg19"
     analysis_workflow = "balsamic-qc"
     snakefile = get_snakefile(workflow, analysis_workflow, reference_genome)
     config_json = tumor_only_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for QC, hg19-tumor-only should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_qc_normal(tumor_normal_config):
-    # GIVEN a sample config dict and snakefile
+def test_workflow_qc_tumor_normal_hg19(tumor_normal_config):
+    # GIVEN a sample config dict and a snakefile
     workflow = "paired"
     reference_genome = "hg19"
     analysis_workflow = "balsamic-qc"
     snakefile = get_snakefile(workflow, analysis_workflow, reference_genome)
     config_json = tumor_normal_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for QC, hg19-tumor-normal should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
 
-def test_workflow_qc_normal_canfam3(tumor_normal_config):
-    # GIVEN a sample config dict and snakefile
+def test_workflow_qc_tumor_only_canfam3(tumor_only_config):
+
+    # GIVEN a sample config dict and a snakefile
+    workflow = "single"
+    reference_genome = "canfam3"
+    analysis_workflow = "balsamic-qc"
+    snakefile = get_snakefile(workflow, analysis_workflow, reference_genome)
+    config_json = tumor_only_config
+
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for QC, canfam3-tumor-only should run successfully.
+    with mock.patch.dict(
+        MOCKED_OS_ENVIRON,
+    ):
+        assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
+
+
+def test_workflow_qc_tumor_normal_canfam3(tumor_normal_config):
+    # GIVEN a sample config dict and a snakefile
     workflow = "paired"
     reference_genome = "canfam3"
     analysis_workflow = "balsamic-qc"
     snakefile = get_snakefile(workflow, analysis_workflow, reference_genome)
     config_json = tumor_normal_config
 
-    # WHEN invoking snakemake module with dryrun option
-    # THEN it should return true
+    # WHEN invoking snakemake module with dry run option
+    # THEN the snakemake workflow for QC, canfam3-tumor-normal should run successfully.
     with mock.patch.dict(
         MOCKED_OS_ENVIRON,
     ):
