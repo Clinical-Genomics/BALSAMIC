2e5c0fcc9f24f9d190a5dad66b7da79aabeda0a1
Khurram Maqbool <5569347+khurrammaqbool@users.noreply.github.com>
2023-09-14 12:24:28 +0200

    feat: add cnvkit container (#1252)
    * add cnvkit container
    
    * changelog
    
    * remove extra space

---
+++ container_tests/cnvkit/cnvkit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Test if commands exist
+
+valid_commands=( "cnvkit.py" "R" )
+
+for valid_command in "${valid_commands[@]}"
+do
+  if ! command -v "${valid_command}" &> /dev/null
+  then
+    echo "${valid_command} could not be found"
+    exit 1
+  else
+    echo "${valid_command} command is found and valid"
+  fi
+done
+
+
