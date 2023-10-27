7b1c9e5aa82d60bd3360e51ebb0c1cb181e910b7
Vadym <vadym.ivanchuk@scilifelab.se>
2023-10-27 15:46:42 +0200

    Fix command

--- BALSAMIC/models/snakemake.py
+++ BALSAMIC/models/snakemake.py
@@ -166,7 +166,7 @@ class SnakemakeExecutable(BaseModel):
     def get_command(self) -> str:
         """Return Snakemake command to be submitted."""
         snakemake_command: str = (
-            f"snakemake --notemp -p --rerun-trigger --delete-temp-output mtime "
+            f"snakemake --notemp --delete-temp-output --rerun-trigger mtime -p "
             f"--directory {self.working_dir.as_posix()} "
             f"--snakefile {self.snakefile.as_posix()} "
             f"{self.get_config_files_option()} "
