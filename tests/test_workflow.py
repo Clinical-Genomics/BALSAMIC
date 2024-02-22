eed32d09eb901d1b96c52bc5e2c707fe2cbc2b46
Eva Caceres <eva.caceres@scilifelab.se>
2024-02-22 16:18:45 +0100

    add check to pytest

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,4 +1,5 @@
 from unittest import mock
+import logging
 import snakemake
 
 from BALSAMIC.constants.analysis import AnalysisWorkflow
@@ -8,13 +9,14 @@ MOCKED_OS_ENVIRON = "os.environ"
 
 
 def test_workflow_tumor_only_tga_hg19(
-    tumor_only_config, sentieon_install_dir, sentieon_license
+    tumor_only_config, sentieon_install_dir, sentieon_license, caplog,
 ):
     # GIVEN a sample config dict and a snakefile
     analysis_type = "single"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow)
     config_json = tumor_only_config
+    caplog.set_level(logging.INFO)
 
     # WHEN invoking snakemake module with dry run option
     # THEN the snakemake workflow for TGA, hg19-tumor-only should run successfully.
@@ -27,15 +29,20 @@ def test_workflow_tumor_only_tga_hg19(
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
+        # THEN the following rules should not be included
+        assert "igh_dux4_detection_tumor_only" not in caplog.text
+
 
 def test_workflow_tumor_normal_tga_hg19(
-    tumor_normal_config, sentieon_install_dir, sentieon_license
+    tumor_normal_config, sentieon_install_dir, sentieon_license, caplog,
 ):
     # GIVEN a sample config dict and a snakefile
     analysis_type = "paired"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow)
     config_json = tumor_normal_config
+    caplog.set_level(logging.INFO)
+
 
     # WHEN invoking snakemake module with dry run option
     # THEN the snakemake workflow for TGA, hg19-tumor-normal should run successfully.
@@ -48,15 +55,20 @@ def test_workflow_tumor_normal_tga_hg19(
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
+        # THEN the following rules should not be included
+        assert "igh_dux4_detection_tumor_normal" not in caplog.text
+
+
 
 def test_workflow_tumor_only_wgs_hg19(
-    tumor_only_wgs_config, sentieon_install_dir, sentieon_license
+    tumor_only_wgs_config, sentieon_install_dir, sentieon_license, caplog,
 ):
     # GIVEN a sample config dict and a snakefile
     analysis_type = "single"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow)
     config_json = tumor_only_wgs_config
+    caplog.set_level(logging.INFO)
 
     # WHEN invoking snakemake module with dry run option
     # THEN the snakemake workflow for WGS, hg19-tumor-only should run successfully.
@@ -69,15 +81,19 @@ def test_workflow_tumor_only_wgs_hg19(
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
 
+        # THEN the following rules should be included
+        assert "igh_dux4_detection_tumor_only" in caplog.text
+
 
 def test_workflow_tumor_normal_wgs_hg19(
-    tumor_normal_wgs_config, sentieon_install_dir, sentieon_license
+    tumor_normal_wgs_config, sentieon_install_dir, sentieon_license, caplog,
 ):
     # GIVEN a sample config dict and a snakefile
     analysis_type = "paired"
     analysis_workflow = "balsamic"
     snakefile = get_snakefile(analysis_type, analysis_workflow)
     config_json = tumor_normal_wgs_config
+    caplog.set_level(logging.INFO)
 
     # WHEN invoking snakemake module with dry run option
     # THEN the snakemake workflow for WGS, hg19-tumor-normal should run successfully.
@@ -89,6 +105,9 @@ def test_workflow_tumor_normal_wgs_hg19(
         },
     ):
         assert snakemake.snakemake(snakefile, configfiles=[config_json], dryrun=True)
+
+        # THEN the following rules should be included
+        assert "igh_dux4_detection_tumor_normal" in caplog.text
 
 
 def test_workflow_qc_tumor_only_hg19(
