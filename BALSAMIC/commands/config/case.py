e011db48a808a731a089c9eff19deb4054c899f4
Mathias Johansson <math.joh.bio@gmail.com>
2024-07-02 11:32:07 +0200

    fix bug

--- BALSAMIC/commands/config/case.py
+++ BALSAMIC/commands/config/case.py
@@ -191,7 +191,7 @@ def case_config(
         directory.mkdir(exist_ok=True)
 
     config_collection_dict = ConfigModel(
-        sentieon_install_dir=sentieon_install_dir.as_posix(),
+        sentieon_install_dir=sentieon_install_dir,
         QC={
             "quality_trim": quality_trim,
             "adapter_trim": adapter_trim,
