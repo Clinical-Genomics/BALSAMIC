aad3daa07f2fbd81905511f61a3b0fd44a74b751
Mathias Johansson <math.joh.bio@gmail.com>
2024-07-10 15:05:00 +0200

    black

--- tests/models/test_params_models.py
+++ tests/models/test_params_models.py
@@ -28,8 +28,9 @@ def test_params_sentieon_wgs_metrics():
 
     # GIVEN Manta params
     test_sentieon_wgs_metrics_params = {
-            "min_base_qual": 10,
-            "cov_threshold": [50, 100, 150, 200, 250],}
+        "min_base_qual": 10,
+        "cov_threshold": [50, 100, 150, 200, 250],
+    }
 
     # WHEN building the model
     test_sentieon_wgs_metrics_built = ParamsSentieonWGSMetrics(
