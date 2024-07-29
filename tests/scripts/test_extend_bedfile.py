6795db73338a57d4c991903781a6461745a76f06
Mathias Johansson <math.joh.bio@gmail.com>
2024-07-29 18:53:31 +0200

    fix bugs

--- tests/scripts/test_extend_bedfile.py
+++ tests/scripts/test_extend_bedfile.py
@@ -15,7 +15,7 @@ def test_extend_bedfile(
     # GIVEN an input bedfile with regions smaller than minimum region size
 
     # GIVEN a minimum region size
-    minimum_region_size = WORKFLOW_PARAMS["expand_short_bedregions"][
+    minimum_region_size = WORKFLOW_PARAMS["bed_pre_processing"][
         "minimum_region_size"
     ]
 
@@ -53,7 +53,7 @@ def test_extend_bedfile(
     assert lines_before[0] == lines_after[0]
 
     # THEN the second row should be extended by 99 bases
-    assert lines_before[1] == [1, 10005000, 10005001]
-    assert lines_after[1] == [1, 10004950, 10005050]
-    region_size = lines_after[1][2] - lines_after[1][1]
+    assert lines_before[1] == ['1', '10005000', '10005001']
+    assert lines_after[1] == ['1', '10004950', '10005050']
+    region_size = int(lines_after[1][2]) - int(lines_after[1][1])
     assert region_size == minimum_region_size
