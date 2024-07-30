884659c62dccce4053688b622d4ee16877fbbf9d
Mathias Johansson <math.joh.bio@gmail.com>
2024-07-30 11:57:29 +0200

    update from parent branch

--- BALSAMIC/assets/scripts/extend_bedfile.py
+++ BALSAMIC/assets/scripts/extend_bedfile.py
@@ -5,7 +5,7 @@ import click
 @click.argument("input_bedfile", type=click.Path(exists=True))
 @click.argument("output_bedfile", type=click.Path())
 @click.option("--extend-to-min-region-size", default=100, help="Will extend regions shorter than the specified size to this minimum size.")
-def extend_bedfile(input_bedfile: str, output_bedfile: str, min_region_size: int):
+def extend_bedfile(input_bedfile: str, output_bedfile: str, extend_to_min_region_size: int):
     """
     Process a BED file to ensure regions are at least a minimum size.
 
@@ -23,12 +23,12 @@ def extend_bedfile(input_bedfile: str, output_bedfile: str, min_region_size: int
             end = int(fields[2])
 
             region_length: int = end - start
-            if region_length < min_region_size:
+            if region_length < extend_to_min_region_size:
                 center = (start + end) // 2
-                half_size = min_region_size // 2
+                half_size = extend_to_min_region_size // 2
                 start = max(0, center - half_size)
                 end = center + half_size
-                if min_region_size % 2 != 0:
+                if extend_to_min_region_size % 2 != 0:
                     end += 1
 
             outfile.write(f"{chrom}\t{start}\t{end}\n")
