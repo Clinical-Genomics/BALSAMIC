40e63e2bef0872fc45282759c6789067eeb8a280
Mathias Johansson <math.joh.bio@gmail.com>
2024-09-18 15:24:34 +0200

    change file delivery tags

---
+++ tests/scripts/test_modify_tnscope_infofield.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from BALSAMIC.assets.scripts.modify_tnscope_infofield import process_vcf
+
+
+def test_modify_tnscope_infofield(tmp_path, cli_runner):
+    """test cli command is properly executed  and returns output file"""
+    pass
