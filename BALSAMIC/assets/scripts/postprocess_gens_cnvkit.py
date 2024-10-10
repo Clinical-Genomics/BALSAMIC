b86d6f455c289b3ff1d30393dbf3147cbb2ccb35
Mathias Johansson <math.joh.bio@gmail.com>
2024-10-10 13:24:13 +0200

    remove purity adjustment

--- BALSAMIC/assets/scripts/postprocess_gens_cnvkit.py
+++ BALSAMIC/assets/scripts/postprocess_gens_cnvkit.py
@@ -1,7 +1,14 @@
 import click
 from BALSAMIC.utils.io import read_csv, write_list_of_strings
 
+
 def calculate_log2_ratio(purity, log2_ratio, ploidy):
+    """Adjuts log2 ratio according to purity and ploidy.
+
+    Based on method in PureCN: https://github.com/lima1/PureCN/issues/40
+
+    Method is not used currently due to questionable results.
+    """
     # Ensure that the inputs are within valid ranges
     if not (0 <= purity <= 1):
         raise ValueError("Purity must be between 0 and 1")
@@ -9,7 +16,7 @@ def calculate_log2_ratio(purity, log2_ratio, ploidy):
     if ploidy <= 0:
         raise ValueError("Ploidy must be a positive number")
 
-    # Ploidy and purity adjustment formula reference to PureCN issue: https://github.com/lima1/PureCN/issues/40
+    # Ploidy and purity adjustment formula
     log2_adjusted = (
         purity * log2_ratio * ploidy + 2 * (1 - purity) * log2_ratio - 2 * (1 - purity)
     ) / (purity * ploidy)
@@ -75,15 +82,19 @@ def create_gens_cov_file(
 
         # adjust log2 ratio
         log2: float = float(row["log2"])
-        log2: float = calculate_log2_ratio(purity, log2, ploidy)
-        log2: float = round(log2, 4)
+
+        # De-activate purity and ploidy adjustment
+        # log2: float = calculate_log2_ratio(purity, log2, ploidy)
+        # log2: float = round(log2, 4)
 
         # store values in list
         log2_data.append(f"{row['chromosome']}\t{midpoint - 1}\t{midpoint}\t{log2}")
 
     # Write log2 data to output file
     resolutions = ["o", "a", "b", "c", "d"]
-    resolution_log2_lines = [f"{resolution}_{line}" for resolution in resolutions for line in log2_data]
+    resolution_log2_lines = [
+        f"{resolution}_{line}" for resolution in resolutions for line in log2_data
+    ]
     write_list_of_strings(resolution_log2_lines, output_file)
 
 
