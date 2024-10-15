7abcfdde2a1eb03a26aa5f757cce8c679a3c8995
Mathias Johansson <math.joh.bio@gmail.com>
2024-10-15 11:54:56 +0200

    fix strictness of panelname

--- BALSAMIC/assets/scripts/collect_qc_metrics.py
+++ BALSAMIC/assets/scripts/collect_qc_metrics.py
@@ -144,8 +144,10 @@ def get_qc_supported_capture_kit(capture_kit, metrics: List[str]) -> str:
         if k != "default":
             available_panel_beds.append(k)
 
-    return next((i for i in available_panel_beds if i in capture_kit), None)
-
+    return next(
+        (i for i in available_panel_beds if re.search(rf'{re.escape(i)}(?=_\d)', capture_kit)),
+        None
+    )
 
 def get_requested_metrics(config: dict, metrics: dict) -> dict:
     """Parses the defined and requested metrics and returns them as a dictionary"""
