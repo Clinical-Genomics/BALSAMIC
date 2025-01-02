d7f00f20d2b1ffddfedfcbf6d61f318910dba59b
Mathias Johansson <math.joh.bio@gmail.com>
2025-01-02 13:24:41 +0100

    update script

--- BALSAMIC/assets/scripts/sex_check.py
+++ BALSAMIC/assets/scripts/sex_check.py
@@ -112,7 +112,7 @@ def predict_sex(cnn_file):
     data_rows = read_cov(cnn_file)
     data_df = process_data(data_rows)
     if not isinstance(data_df, pd.DataFrame):
-        predicted_sex["sex_prediction"] = "Failed"
+        predicted_sex["sex_prediction"] = "failed"
         predicted_sex["data_dict"] = "NA"
         return predicted_sex
 
@@ -139,33 +139,119 @@ def predict_sex(cnn_file):
     return predicted_sex
 
 def get_prediction(prediction):
-    if "Failed" in prediction["sex_prediction"]:
-        return "Failed", "NA", "Failed", "NA"
+    if "failed" in prediction["sex_prediction"]:
+        return "NA", "NA", "NA", "NA"
     else:
         return prediction["sex_prediction"]["by_mean"]["predicted_sex"], prediction["sex_prediction"]["by_mean"]["predicted_sex_confidence"], prediction["sex_prediction"]["by_median"]["predicted_sex"], prediction["sex_prediction"]["by_median"]["predicted_sex_confidence"]
 
 
-def summarise_sample_sex_prediction(target_predicted_sex, antitarget_predicted_sex):
-    sample_predicted_sex = {}
+def calculate_prediction_score(sex_prediction):
+    frac_conf_score = {
+        "high": 3,
+        "medium": 2,
+        "low": 1,
+        "NA": 0,
+    }
+    data_conf_score = {
+        "high": 5,
+        "medium": 4,
+        "low": 1,
+        "NA": 0,
+    }
+    data_type_score = {
+        "target": 1,
+        "antitarget": 0,
+    }
+    frac_conf = sex_prediction["frac_conf"]
+    data_conf = sex_prediction["data_conf"]
+    data_type = sex_prediction["data_type"]
+    score = frac_conf_score[frac_conf] + data_conf_score[data_conf] + data_type_score[data_type]
+    return score
 
+def consolidate_sex_predictions(sex_predictions):
 
-    # target_data_amount = target_predicted_sex["data_dict"]["data_amount"]
+    final_sex = "NA"
+    final_score = 0
+
+    for sex_prediction in sex_predictions:
+        sex = sex_prediction["sex"]
+        score = sex_prediction["score"]
+        data_type = sex_prediction["data_type"]
+
+        # Set sex based on highest score
+        if score > final_score:
+            final_sex = sex
+            final_score = score
+        elif score == final_score:
+            # If score is the same, prioritise target data
+            if data_type == "target":
+                final_sex = sex
+
+    score_confidence_levels = {
+        "high": 6,
+        "medium": 5,
+        "low": 3,
+    }
+    final_confidence = "uncertain"
+    if final_score > score_confidence_levels["low"]:
+        final_confidence = "low"
+    if final_score > score_confidence_levels["medium"]:
+        final_confidence = "medium"
+    if final_score > score_confidence_levels["high"]:
+        final_confidence = "high"
+
+    return {"sex": final_sex, "sex_score": score, "prediction_confidence": final_confidence}
+
+def summarise_sample_sex_prediction(target_predicted_sex, antitarget_predicted_sex, sample_type):
+    sample_predicted_sex = {sample_type: {}}
+
+    if "failed" in target_predicted_sex["sex_prediction"]:
+        target_data_amount = "NA"
+    else:
+        target_data_amount = target_predicted_sex["data_dict"]["data_amount"]
+
     mean_target_predicted_sex, mean_target_predicted_sex_conf, median_target_predicted_sex,  median_target_predicted_sex_conf = get_prediction(target_predicted_sex)
 
-    # antitarget_data_amount = antitarget_predicted_sex["data_dict"]["data_amount"]
+    if "failed" in antitarget_predicted_sex["sex_prediction"]:
+        antitarget_data_amount = "NA"
+    else:
+        antitarget_data_amount = antitarget_predicted_sex["data_dict"]["data_amount"]
+
     mean_antitarget_predicted_sex, mean_antitarget_predicted_sex_conf, median_antitarget_predicted_sex, median_antitarget_predicted_sex_conf = get_prediction(antitarget_predicted_sex)
 
+    sample_predicted_sex[sample_type]["predicted_sex"] = [
+        {"sex": mean_target_predicted_sex, "frac_conf": mean_target_predicted_sex_conf, "data_conf": target_data_amount, "data_type": "target"},
+        {"sex": median_target_predicted_sex, "frac_conf": median_target_predicted_sex_conf, "data_conf": target_data_amount, "data_type": "target"},
+        {"sex": mean_antitarget_predicted_sex, "frac_conf": mean_antitarget_predicted_sex_conf, "data_conf": antitarget_data_amount, "data_type": "antitarget"},
+        {"sex": median_antitarget_predicted_sex, "frac_conf": median_antitarget_predicted_sex_conf, "data_conf": antitarget_data_amount, "data_type": "antitarget"},
+    ]
 
-    sample_predicted_sex["predicted_sex"] = [mean_target_predicted_sex, median_target_predicted_sex, mean_antitarget_predicted_sex, median_antitarget_predicted_sex]
-    sample_predicted_sex["predicted_sex_conf"] = [mean_target_predicted_sex_conf, median_target_predicted_sex_conf, mean_antitarget_predicted_sex_conf, median_antitarget_predicted_sex_conf]
+    for idx, sex_prediction in enumerate(sample_predicted_sex[sample_type]["predicted_sex"]):
+        score = calculate_prediction_score(sex_prediction)
+        sample_predicted_sex[sample_type]["predicted_sex"][idx].update({"score": score})
 
+    sample_predicted_sex[sample_type]["final_sex"] = consolidate_sex_predictions(sample_predicted_sex[sample_type]["predicted_sex"])
 
-    sample_predicted_sex["target_predicted_sex"] = target_predicted_sex
-    sample_predicted_sex["antitarget_predicted_sex"] = antitarget_predicted_sex
+    # Add raw data
+    sample_predicted_sex[sample_type]["target_predicted_sex"] = target_predicted_sex
+    sample_predicted_sex[sample_type]["antitarget_predicted_sex"] = antitarget_predicted_sex
 
     return sample_predicted_sex
-def compare_tumor_normal_sex_prediction():
-    pass
+
+def case_sex_prediction(predicted_sex):
+    predicted_sex["case_sex"] = {}
+    tumor_sex = predicted_sex["tumor"]["final_sex"]["sex"]
+    if "normal" in predicted_sex:
+        normal_sex = predicted_sex["normal"]["final_sex"]["sex"]
+        if tumor_sex != normal_sex:
+            case_sex = "conflicting"
+        else:
+            case_sex = tumor_sex
+    else:
+        case_sex = tumor_sex
+
+    predicted_sex["case_sex"] = case_sex
+    return predicted_sex
 
 def write_json(json_obj: dict, path: str) -> None:
     """Write JSON format data to an output file."""
@@ -189,11 +275,19 @@ def process_files(target_cnn_tumor, antitarget_cnn_tumor, output, target_cnn_nor
 
     tumor_target_predicted_sex = predict_sex(target_cnn_tumor)
     tumor_antitarget_predicted_sex = predict_sex(antitarget_cnn_tumor)
-    sample_predicted_sex = summarise_sample_sex_prediction(tumor_target_predicted_sex, tumor_antitarget_predicted_sex)
+    predicted_sex = summarise_sample_sex_prediction(tumor_target_predicted_sex, tumor_antitarget_predicted_sex, "tumor")
 
-    write_json(sample_predicted_sex, output)
+    if target_cnn_normal:
+        normal_target_predicted_sex = predict_sex(target_cnn_normal)
+        normal_antitarget_predicted_sex = predict_sex(antitarget_cnn_normal)
+        normal_predicted_sex = summarise_sample_sex_prediction(normal_target_predicted_sex, normal_antitarget_predicted_sex, "normal")
+        predicted_sex.update(normal_predicted_sex)
 
-    #antitarget_cnn_tumor
+    # Create case-level prediction (compare tumor and normal sex)
+    predicted_sex = case_sex_prediction(predicted_sex)
+
+    # Write json report
+    write_json(predicted_sex, output)
 
 
 
