8b67ee74e6c1da1344b76eab09526dbad8e7995b
Mathias Johansson <math.joh.bio@gmail.com>
2025-01-09 14:28:06 +0100

    fix issues

--- tests/scripts/test_collect_qc_metrics.py
+++ tests/scripts/test_collect_qc_metrics.py
@@ -322,9 +322,7 @@ def test_get_sex_check_metrics(tga_male_sex_prediction, config_dict):
     ]
 
     # WHEN comparing predicted male sex and supplied male gender
-    sex_metrics: list = get_sex_check_metrics(
-        tga_male_sex_prediction, config_dict
-    )
+    sex_metrics: list = get_sex_check_metrics(tga_male_sex_prediction, config_dict)
 
     # THEN check that the relatedness metrics has been correctly shaped
     assert sex_metrics == expected_sex_check_metric
