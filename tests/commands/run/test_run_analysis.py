2c6d18ce8780bc860e2034553dd641c0592cd028
Mathias Johansson <math.joh.bio@gmail.com>
2025-05-12 11:50:03 +0200

    black

--- tests/commands/run/test_run_analysis.py
+++ tests/commands/run/test_run_analysis.py
@@ -22,6 +22,7 @@ def test_run_analysis_tumor_normal_dry_run(invoke_cli, tumor_normal_config):
     # THEN it should run without any error
     assert result.exit_code == 0
 
+
 def test_run_analysis_tumor_only_dry_run(invoke_cli, tumor_only_config):
     # GIVEN a tumor-only config file
     # WHEN running analysis
