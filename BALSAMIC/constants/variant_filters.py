2a1ef2aeaa0262f0f230b0e284df320eee7a01d8
Mathias Johansson <math.joh.bio@gmail.com>
2025-06-05 17:06:55 +0200

    add filters

--- BALSAMIC/constants/variant_filters.py
+++ BALSAMIC/constants/variant_filters.py
@@ -467,6 +467,11 @@ SVDB_FILTER_SETTINGS = {
         "filter_name": "Frq",
         "field": "INFO",
     },
+    "loqusdb_artefact_sv_obs": {
+        "tag_value": 5,
+        "filter_name": "ArtefactObs",
+        "field": "INFO",
+    },
     "varcaller_name": "svdb",
     "filter_type": "general",
     "analysis_type": "tumor_only,tumor_normal",
