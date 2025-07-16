3b617eef57940a0a13c3ef653101b0a36aea81e5
Mathias Johansson <math.joh.bio@gmail.com>
2025-07-16 11:19:08 +0200

    fix

--- BALSAMIC/utils/metrics.py
+++ BALSAMIC/utils/metrics.py
@@ -2,6 +2,6 @@
 from BALSAMIC.models.metrics import MetricValidation
 
 
-def validate_qc_metrics(metrics: dict, LOG) -> dict:
+def validate_qc_metrics(metrics: dict) -> dict:
     """Returns a set of validated QC metrics."""
     return MetricValidation(metrics=metrics).model_dump()["metrics"]
