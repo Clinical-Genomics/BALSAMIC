98f60f36edbc1b41b5c5076a90c698b87dc3ddc3
Mathias Johansson <math.joh.bio@gmail.com>
2025-08-20 10:41:45 +0200

    try fix

--- BALSAMIC/models/snakemake.py
+++ BALSAMIC/models/snakemake.py
@@ -145,7 +145,7 @@ class SnakemakeExecutable(BaseModel):
         if self.run_mode == RunMode.CLUSTER:
             snakemake_cluster_options: str = (
                 f"-j {MAX_JOBS} "
-                f"--profile {self.workflow_profile} --set-resources __default__:slurm_account={self.account} --set-resources __default__:slurm_qos={self.qos} --slurm-keep-successful-logs"
+                f"--profile {self.workflow_profile} --default-resources slurm_qos={self.qos} slurm_partition=core slurm_account={self.account} --slurm-keep-successful-logs"
             )
             return remove_unnecessary_spaces(snakemake_cluster_options)
         return "--default-resources mem_mb=32000 threads=8"
