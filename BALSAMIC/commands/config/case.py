563727bc774776831f6b393691898290aba4676c
Mathias Johansson <math.joh.bio@gmail.com>
2025-12-19 15:45:17 +0100

    fix

--- BALSAMIC/commands/config/case.py
+++ BALSAMIC/commands/config/case.py
@@ -261,7 +261,7 @@ def case_config(
             "analysis_workflow": analysis_workflow,
             "config_creation_date": datetime.now().strftime("%Y-%m-%d %H:%M"),
             "rescue_snvs": rescue_snvs if rescue_snvs else RESCUE_SNVS.as_posix(),
-            "cancer_genelist": cancer_genelist.as_posix(),
+            "cancer_genelist": cancer_genelist,
         },
         custom_filters={"umi_min_reads": umi_min_reads if umi_min_reads else None},
         reference=references,
