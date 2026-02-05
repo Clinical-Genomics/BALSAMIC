9c77fa9559edcc19f3fbe3f6df0f1dbb8528f12e
beatrizsavinhas <beatriz.sa.vinhas@gmail.com>
2026-02-05 16:24:29 +0100

    Clean up imports

--- BALSAMIC/commands/init/base.py
+++ BALSAMIC/commands/init/base.py
@@ -6,10 +6,9 @@ import sys
 import shlex
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional, Union, Dict
+from typing import List, Optional, Dict
 
 import click
-from pydantic.config import ExtraValues
 
 from BALSAMIC.commands.options import (
     OPTION_WORKFLOW_PARTITION,
